d42616c103c7207a84abe5037fc7cf53224cd694
mattcompiles <mattjones701@gmail.com>
2024-08-20 15:14:15 +1000

    Fix unit tests

--- packages/core/core/test/test-utils.js
+++ packages/core/core/test/test-utils.js
@@ -5,7 +5,6 @@ import type {Environment, AtlaspackOptions, Target} from '../src/types';
 import {DEFAULT_FEATURE_FLAGS} from '@atlaspack/feature-flags';
 import {FSCache} from '@atlaspack/cache';
 import tempy from 'tempy';
-import path from 'path';
 import {inputFS, outputFS} from '@atlaspack/test-utils';
 import {relativePath} from '@atlaspack/utils';
 import {NodePackageManager} from '@atlaspack/package-manager';
@@ -17,7 +16,7 @@ export let cache: FSCache = new FSCache(outputFS, cacheDir);
 cache.ensure();
 
 export const DEFAULT_OPTIONS: AtlaspackOptions = {
-  cacheDir: path.join(__dirname, '.atlaspack-cache'),
+  cacheDir,
   atlaspackVersion: '',
   watchDir: __dirname,
   watchIgnore: undefined,
