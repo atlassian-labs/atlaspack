885c59187f7b1575074ab95f0fbf1eda4e351c41
Monica Olejniczak <molejniczak@atlassian.com>
2024-08-21 13:10:49 +1000

    Remove first class support for less

--- packages/core/integration-tests/test/less.js
+++ packages/core/integration-tests/test/less.js
@@ -1,19 +1,41 @@
 import assert from 'assert';
-import path from 'path';
+import {join} from 'path';
 import {
   assertBundles,
   bundle,
   describe,
   distDir,
+  fsFixture,
   it,
   outputFS,
+  overlayFS,
   run,
 } from '@atlaspack/test-utils';
 import {md} from '@atlaspack/diagnostic';
 
 describe.v2('less', function () {
   it('should support requiring less files', async function () {
-    let b = await bundle(path.join(__dirname, '/integration/less/index.js'));
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.js:
+        require('./index.less');
+
+      index.less:
+        @base: #f938ab;
+
+        .index {
+          color: @base;
+        }
+    `;
+
+    let b = await bundle('index.js', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -26,35 +48,49 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(css.includes('.index'));
   });
 
   it('should support less imports', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-import/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      a.less:
+        .a { color: red }
+
+      b.less:
+        .b { color: orange }
+
+      c.less:
+        .c { color: yellow }
+
+      d.less:
+        .d { color: green }
+
+      index.less:
+        @import './a.less';
+        @import 'b.less';
+        @import './c';
+        @import 'd';
+    `;
+
+    let b = await bundle('index.less', {inputFS: overlayFS});
 
     assertBundles(b, [
-      {
-        name: 'index.js',
-        assets: ['index.js'],
-      },
       {
         name: 'index.css',
         assets: ['index.less'],
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(css.includes('.a'));
     assert(css.includes('.b'));
     assert(css.includes('.c'));
@@ -62,9 +98,62 @@ describe.v2('less', function () {
   });
 
   it('should support advanced less imports', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-advanced-import/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      nested/externals.less:
+        @import '~/node_modules/explicit-external-less/a.less';
+        @import 'external-less';
+        @import 'external-less/a.less';
+        @import 'external-less-with-main';
+
+      node_modules/explicit-external-less/a.less:
+        .explicit-external-a {
+          background: red;
+        }
+
+      node_modules/external-less/a.less:
+        .external-a {
+          background: red;
+        }
+
+      node_modules/external-less/index.less:
+        .external-index {
+          color: red;
+        }
+
+      node_modules/external-less/package.json: {}
+
+      node_modules/external-less-with-main/main.less:
+        .external-with-main {
+          color: red;
+        }
+
+      node_modules/external-less-with-main/package.json:
+        {
+          "main": "main.less"
+        }
+
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      a.less:
+        .a {
+          color: red;
+        }
+
+      index.js:
+        require('~/index.less');
+
+      index.less:
+        @import '~/a.less';
+        @import './nested/externals.less';
+    `;
+
+    let b = await bundle('index.js', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -77,11 +166,7 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
 
     assert(css.includes('.a'));
     assert(css.includes('.external-index'));
@@ -91,9 +176,22 @@ describe.v2('less', function () {
   });
 
   it('should support requiring empty less files', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-empty/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.js:
+        require('./index.less');
+
+      index.less:
+    `;
+
+    let b = await bundle('index.js', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -106,24 +204,36 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert.equal(css.trim(), '/*# sourceMappingURL=index.css.map */');
   });
 
   it('should support linking to assets with url() from less', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-url/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      fonts/test.woff2: test
+
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.less:
+        @font-face {
+          font-family: "Test";
+          src: url("./fonts/test.woff2") format("woff2");
+        }
+
+        .index {
+          background: url("http://google.com");
+        }
+    `;
+
+    let b = await bundle('index.less', {inputFS: overlayFS});
 
     assertBundles(b, [
-      {
-        name: 'index.js',
-        assets: ['index.js'],
-      },
       {
         name: 'index.css',
         assets: ['index.less'],
@@ -134,32 +244,60 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(/url\("?test\.[0-9a-f]+\.woff2"?\)/.test(css));
     assert(/url\("?http:\/\/google.com"?\)/.test(css));
     assert(css.includes('.index'));
 
     assert(
       await outputFS.exists(
-        path.join(distDir, css.match(/url\("?(test\.[0-9a-f]+\.woff2)"?\)/)[1]),
+        join(distDir, css.match(/url\("?(test\.[0-9a-f]+\.woff2)"?\)/)[1]),
       ),
     );
   });
 
   it('should support less url rewrites', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-url-rewrite/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      nested/a.less:
+        @font-face {
+          font-family: "A";
+          src: url("./a.woff2") format("woff2");
+        }
+
+        .a {
+          font-family: "A";
+        }
+
+      nested/a.woff2: test
+
+      node_modules/library/b.less:
+        @font-face {
+          font-family: "B";
+          src: url("./b.woff2") format("woff2");
+        }
+
+        .b {
+          font-family: "B";
+        }
+
+      node_modules/library/b.woff2: test
+
+      index.less:
+        @import "./nested/a.less";
+        @import "./node_modules/library/b.less";
+    `;
+
+    let b = await bundle('index.less', {inputFS: overlayFS});
 
     assertBundles(b, [
-      {
-        name: 'index.js',
-        assets: ['index.js'],
-      },
       {
         name: 'index.css',
         assets: ['index.less'],
@@ -174,19 +312,40 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(css.includes('.a'));
     assert(css.includes('.b'));
   });
 
   it('should support css modules in less', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-postcss/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      img.svg:
+
+      index.js:
+        var map = require('./index.module.less');
+
+        module.exports = function () {
+          return map.index;
+        };
+
+      index.module.less:
+        @base: #f938ab;
+
+        .index {
+          color: @base;
+          background-image: url('img.svg');
+        }
+    `;
+
+    let b = await bundle('index.js', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -206,29 +365,70 @@ describe.v2('less', function () {
     assert.equal(typeof output, 'function');
     assert(output().endsWith('_index'));
 
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(/\.[_0-9a-zA-Z]+_index/.test(css));
   });
 
   it('should throw an exception when using webpack syntax', async function () {
-    await assert.rejects(
-      () =>
-        bundle(
-          path.join(
-            __dirname,
-            '/integration/less-webpack-import-error/index.js',
-          ),
-        ),
-      {
-        message: md`The @import path "${'~library/style.less'}" is using webpack specific syntax, which isn't supported by Atlaspack.\n\nTo @import files from ${'node_modules'}, use "${'library/style.less'}"`,
-      },
-    );
+    await fsFixture(overlayFS)`
+      node_modules/library/styles.less:
+
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.less:
+        @import '~library/style.less';
+    `;
+
+    await assert.rejects(() => bundle('index.less', {inputFS: overlayFS}), {
+      message: md`The @import path "${'~library/style.less'}" is using webpack specific syntax, which isn't supported by Atlaspack.\n\nTo @import files from ${'node_modules'}, use "${'library/style.less'}"`,
+    });
   });
 
   it('should support configuring less include paths', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-include-paths/index.js'),
-    );
+    await fsFixture(overlayFS)`
+      include-path/a.less:
+        .a {
+          color: red;
+        }
+
+      node_modules/library/b.less:
+        .b {
+          color: red;
+        }
+
+      index.js:
+        require('./index.less');
+
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      .lessrc.js:
+        const { join } = require('path');
+
+        module.exports = {
+          paths: [
+            join(__dirname, 'include-path'),
+            join(__dirname, 'node_modules', 'library')
+          ]
+        };
+
+      index.less:
+        @import 'a.less';
+        @import 'b.less';
+    `;
+
+    let b = await bundle('index.js', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -241,19 +441,28 @@ describe.v2('less', function () {
       },
     ]);
 
-    let output = await run(b);
-    assert.equal(typeof output, 'function');
-    assert.equal(output(), 2);
-
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(css.includes('.a'));
     assert(css.includes('.b'));
   });
 
   it('should ignore url() with IE behavior specifiers', async function () {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-url-behavior/index.less'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.less:
+        .index {
+          behavior: url(#default#VML);
+        }
+    `;
+
+    let b = await bundle('index.less', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -262,15 +471,29 @@ describe.v2('less', function () {
       },
     ]);
 
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
 
     assert(css.includes('url("#default#VML")'));
   });
 
   it('preserves quotes around data urls that require them', async () => {
-    let b = await bundle(
-      path.join(__dirname, '/integration/less-url-quotes/index.less'),
-    );
+    await fsFixture(overlayFS)`
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.less:
+        .index {
+          // Note the literal space after "xml"
+          background: url("data:image/svg+xml,%3C%3Fxml version%3D%221.0%22%3F%3E%3Csvg%3E%3C%2Fsvg%3E");
+        }
+    `;
+
+    let b = await bundle('index.less', {inputFS: overlayFS});
 
     assertBundles(b, [
       {
@@ -279,7 +502,7 @@ describe.v2('less', function () {
       },
     ]);
 
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(
       css.includes(
         // Note the literal space after "xml"
@@ -288,10 +511,44 @@ describe.v2('less', function () {
     );
   });
 
-  it('should support the less package exports condition', async function () {
-    await bundle(path.join(__dirname, '/integration/less-exports/index.less'));
+  it('should support package exports style condition', async function () {
+    await fsFixture(overlayFS)`
+      node_modules/foo/a.less:
+        .a {
+          color: red;
+        }
 
-    let css = await outputFS.readFile(path.join(distDir, 'index.css'), 'utf8');
+      node_modules/foo/package.json:
+        {
+          "exports": {
+            "style": "./a.less"
+          }
+        }
+
+      .atlaspackrc:
+        {
+          "extends": "@atlaspack/config-default",
+          "transformers": {
+            "*.less": ["@atlaspack/transformer-less"]
+          }
+        }
+
+      index.less:
+        @import "foo";
+
+      package.json:
+        {
+          "@atlaspack/resolver-default": {
+            "packageExports": true
+          }
+        }
+
+      yarn.lock: {}
+    `;
+
+    await bundle('index.less', {inputFS: overlayFS});
+
+    let css = await outputFS.readFile(join(distDir, 'index.css'), 'utf8');
     assert(css.includes('.a'));
   });
 });
