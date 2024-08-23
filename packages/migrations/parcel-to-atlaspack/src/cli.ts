31785a045925ad01e053be862c1507082da652dc
Monica Olejniczak <molejniczak@atlassian.com>
2024-08-23 12:20:48 +1000

    Add atlaspack migration package

---
+++ packages/migrations/parcel-to-atlaspack/src/cli.ts
@@ -0,0 +1,58 @@
+import {Command} from 'commander';
+
+import packageJson from '../package.json';
+
+import {migratePackageJson} from './migrations/migrate-package-json';
+import {migrateParcelRc} from './migrations/migrate-parcelrc';
+
+export async function run() {
+  const program = new Command();
+
+  program
+    .name(packageJson.name)
+    .description('Migrate from Parcel to Atlaspack')
+    .option(
+      '--cwd <cwd>',
+      'The current working directory that the script will run the migration on',
+      process.cwd(),
+    )
+    .option(
+      '--dry-run',
+      'Report the changes that will be made instead of making them',
+    )
+    .option(
+      '--skip-dependencies',
+      'Skip migrating the parcel dependencies to atlaspack',
+    )
+    .option(
+      '--skip-engines',
+      'Skip migrating the parcel key in package.json#engines',
+    )
+    .option(
+      '--skip-parcelrc',
+      'Skip migrating the .parcelrc file to .atlaspackrc',
+    )
+    .option(
+      '--tag <tag>',
+      'The tag used to search the npm registry for Atlaspack packages',
+      'canary',
+    )
+    .version(packageJson.version);
+
+  program.parse();
+
+  const {
+    cwd,
+    dryRun,
+    skipDependencies,
+    skipEngines,
+    skipParcelrc: skipParcelRc,
+    tag,
+  } = program.opts();
+
+  // TODO Node API / types, parcel exe in scripts, etc
+  await Promise.all([
+    migratePackageJson({cwd, dryRun, skipDependencies, skipEngines, tag}),
+    !skipParcelRc && migrateParcelRc({cwd, dryRun}),
+  ]);
+}
