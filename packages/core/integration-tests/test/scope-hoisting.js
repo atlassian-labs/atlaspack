08815ab81846607b8000972a558cc399a9a7b56f
Marcin Szczepanski <mszczepanski@atlassian.com>
2024-09-05 16:41:32 +1000

    Skip test that uses HTML entry for v3

--- packages/core/integration-tests/test/scope-hoisting.js
+++ packages/core/integration-tests/test/scope-hoisting.js
@@ -6318,8 +6318,11 @@ describe('scope hoisting', function () {
     }
   });
 
-  it('should allow force skipping of wrapping assets in async bundles', async function () {
-    await fsFixture(overlayFS, __dirname)`
+  // Will work in v3 once HTML entries are supported
+  it.v2(
+    'should allow force skipping of wrapping assets in async bundles',
+    async function () {
+      await fsFixture(overlayFS, __dirname)`
       force-skip-wrap
         index.html:
           <script type="module" src="./index.js"></script>
@@ -6355,53 +6358,57 @@ describe('scope hoisting', function () {
           }
         yarn.lock:`;
 
-    let b = await bundle([path.join(__dirname, 'force-skip-wrap/index.html')], {
-      mode: 'production',
-      defaultTargetOptions: {
-        shouldScopeHoist: true,
-        shouldOptimize: true,
-        outputFormat: 'esmodule',
-      },
-      inputFS: overlayFS,
-    });
-    function hasAsset(bundle, assetName) {
-      let result = false;
+      let b = await bundle(
+        [path.join(__dirname, 'force-skip-wrap/index.html')],
+        {
+          mode: 'production',
+          defaultTargetOptions: {
+            shouldScopeHoist: true,
+            shouldOptimize: true,
+            outputFormat: 'esmodule',
+          },
+          inputFS: overlayFS,
+        },
+      );
+      function hasAsset(bundle, assetName) {
+        let result = false;
 
-      bundle.traverseAssets(asset => {
-        if (asset.filePath.includes(assetName)) {
-          result = true;
-        }
+        bundle.traverseAssets(asset => {
+          if (asset.filePath.includes(assetName)) {
+            result = true;
+          }
+        });
+
+        return result;
+      }
+      let asyncBundleContents = await outputFS.readFile(
+        nullthrows(
+          b.getBundles().find(b => hasAsset(b, 'async-root.js')),
+          'No async bundle',
+        ).filePath,
+        'utf8',
+      );
+      assert(
+        asyncBundleContents.includes(
+          '{one:{name:"one",value:1},two:{name:"two",value:2}}',
+        ),
+        "Async bundle should directly reference the 'one' asset into the base asset",
+      );
+
+      let resolve;
+      let p = new Promise(r => {
+        resolve = r;
       });
-
-      return result;
-    }
-    let asyncBundleContents = await outputFS.readFile(
-      nullthrows(
-        b.getBundles().find(b => hasAsset(b, 'async-root.js')),
-        'No async bundle',
-      ).filePath,
-      'utf8',
-    );
-    assert(
-      asyncBundleContents.includes(
-        '{one:{name:"one",value:1},two:{name:"two",value:2}}',
-      ),
-      "Async bundle should directly reference the 'one' asset into the base asset",
-    );
-
-    let resolve;
-    let p = new Promise(r => {
-      resolve = r;
-    });
-    await run(b, {
-      result: r => {
-        resolve(r);
-      },
-    });
-    const result = await p;
-    assert.deepEqual(result, {
-      one: {name: 'one', value: 1},
-      two: {name: 'two', value: 2},
-    });
-  });
+      await run(b, {
+        result: r => {
+          resolve(r);
+        },
+      });
+      const result = await p;
+      assert.deepEqual(result, {
+        one: {name: 'one', value: 1},
+        two: {name: 'two', value: 2},
+      });
+    },
+  );
 });
