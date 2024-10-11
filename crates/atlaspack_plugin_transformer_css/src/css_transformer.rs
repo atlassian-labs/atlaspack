1faa4e5262881affed641bf2b59daa4a259b3e1a
mattcompiles <mattjones701@gmail.com>
2024-10-11 15:53:19 +1100

    Dependency handling

--- crates/atlaspack_plugin_transformer_css/src/css_transformer.rs
+++ crates/atlaspack_plugin_transformer_css/src/css_transformer.rs
@@ -1,13 +1,14 @@
 use std::ops::Deref;
-use std::path::PathBuf;
 use std::sync::Arc;
 
 use anyhow::{anyhow, Error};
 use atlaspack_core::plugin::{PluginContext, TransformerPlugin};
 use atlaspack_core::plugin::{TransformContext, TransformResult};
 use atlaspack_core::types::engines::Engines;
-use atlaspack_core::types::{Asset, BundleBehavior, Code, EnvironmentContext};
-use lightningcss::dependencies::DependencyOptions;
+use atlaspack_core::types::{
+  Asset, Code, Dependency, EnvironmentContext, ExportsCondition, FileType, Priority, SpecifierType,
+};
+use lightningcss::dependencies::{self, Dependency, DependencyOptions};
 use lightningcss::printer::PrinterOptions;
 use lightningcss::stylesheet::{self, ParserFlags, ParserOptions, StyleSheet};
 use lightningcss::targets::Targets;
@@ -31,7 +32,15 @@ impl TransformerPlugin for AtlaspackCssTransformerPlugin {
     context: TransformContext,
     asset: Asset,
   ) -> Result<TransformResult, Error> {
-    let result = StyleSheet::parse(
+    // TODO: Hardcoded AFM defaults for now, could support proper config here if
+    // need be
+    let css_modules = if asset.is_source {
+      Some(Default::default())
+    } else {
+      None
+    };
+
+    let stylesheet = StyleSheet::parse(
       asset.code.as_str()?,
       ParserOptions {
         filename: asset
@@ -40,62 +49,116 @@ impl TransformerPlugin for AtlaspackCssTransformerPlugin {
           .into_os_string()
           .into_string()
           .map_err(|_e| anyhow!("Couldn't convert file path to String"))?,
-        css_modules: None,
+        css_modules: Some(Default::default()),
         source_index: Default::default(),
         error_recovery: false,
         warnings: None,
         flags: ParserFlags::empty(),
       },
-    );
+    )
+    .map_err(|err| {
+      // TODO: Proper error handling
+      anyhow!("Failed to parse CSS {}", asset.file_path.display())
+    })?;
 
-    match result {
-      Err(_err) => {
-        // TODO: Workout how to handle the code lifetime for errors
-        Err(anyhow!("failure"))
-      }
-      Ok(stylesheet) => {
-        let mut asset = asset.clone();
+    let mut asset = asset.clone();
 
-        //  Normalize the asset's environment so that properties that only affect JS don't cause CSS to be duplicated.
-        // For example, with ESModule and CommonJS targets, only a single shared CSS bundle should be produced.
-        asset.env = Arc::new(atlaspack_core::types::Environment {
-          context: EnvironmentContext::Browser,
-          engines: Engines {
-            browsers: asset.env.engines.browsers.clone(),
-            ..Default::default()
-          },
-          ..asset.env.deref().clone()
-        });
+    // Normalize the asset's environment so that properties that only affect JS don't cause CSS to be duplicated.
+    // For example, with ESModule and CommonJS targets, only a single shared CSS bundle should be produced.
+    asset.env = Arc::new(atlaspack_core::types::Environment {
+      context: EnvironmentContext::Browser,
+      engines: Engines {
+        browsers: asset.env.engines.browsers.clone(),
+        ..Default::default()
+      },
+      ..asset.env.deref().clone()
+    });
 
-        let preserve_imports = asset
-          .meta
-          .get("hasDependencies")
-          .map_or_else(|| true, |value| value != false);
+    let preserve_imports = asset
+      .meta
+      .get("hasDependencies")
+      .map_or_else(|| true, |value| value != false);
 
-        let css = stylesheet.to_css(PrinterOptions {
-          minify: false,
-          source_map: None,
-          project_root: self.project_root.to_str(),
-          targets: Targets {
-            // TODO: provide this correctly
-            browsers: None,
-            include: Default::default(),
-            exclude: Default::default(),
-          },
-          analyze_dependencies: Some(DependencyOptions {
-            remove_imports: !preserve_imports,
-          }),
-          pseudo_classes: None,
-        })?;
+    let css = stylesheet.to_css(PrinterOptions {
+      minify: false,
+      source_map: None,
+      project_root: self.project_root.to_str(),
+      targets: Targets {
+        // TODO: provide this correctly
+        browsers: None,
+        include: Default::default(),
+        exclude: Default::default(),
+      },
+      analyze_dependencies: Some(DependencyOptions {
+        remove_imports: !preserve_imports,
+      }),
+      pseudo_classes: None,
+    })?;
 
-        asset.code = Arc::new(Code::from(css.code));
+    asset.code = Arc::new(Code::from(css.code));
 
-        Ok(TransformResult {
-          asset,
-          ..Default::default()
-        })
-      }
-    }
+    let dependencies = css
+      .dependencies
+      .map(|dependencies| {
+        dependencies
+          .iter()
+          .filter_map(|dependency| match dependency {
+            lightningcss::dependencies::Dependency::Import(import_dependency) => {
+              if css.exports.is_some() {
+                // When exports from a CSS module are available, we handle the
+                // dependencies separately
+                return None;
+              }
+
+              let dependency = Dependency {
+                env: asset.env.clone(),
+                priority: Priority::Sync,
+                source_asset_id: Some(asset.id.clone()),
+                source_asset_type: Some(FileType::Css),
+                source_path: Some(asset.file_path.clone()),
+                specifier: import_dependency.url,
+                specifier_type: SpecifierType::Url,
+                package_conditions: ExportsCondition::STYLE,
+                ..Dependency::default()
+              };
+
+              if let Some(media) = import_dependency.media {
+                dependency.meta.insert("media".into(), media.into());
+              }
+
+              // For the glob resolver to distinguish between `@import` and other URL dependencies.
+              dependency.meta.insert("isCSSImport".into(), true.into());
+
+              dependency.set_placeholder(import_dependency.placeholder);
+
+              Some(dependency)
+            }
+            lightningcss::dependencies::Dependency::Url(url_dependency) => {
+              let dependency = Dependency {
+                env: asset.env.clone(),
+                priority: Priority::Sync,
+                source_asset_id: Some(asset.id.clone()),
+                source_asset_type: Some(FileType::Css),
+                source_path: Some(asset.file_path.clone()),
+                specifier: url_dependency.url,
+                specifier_type: SpecifierType::Url,
+                ..Dependency::default()
+              };
+
+              dependency.set_placeholder(url_dependency.placeholder);
+
+              Some(dependency)
+            }
+          })
+          .collect()
+      })
+      .unwrap_or_default();
+
+    Ok(TransformResult {
+      asset,
+      dependencies,
+      ..Default::default()
+    })
   }
 }
 
