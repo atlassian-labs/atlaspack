fee04114bacb1df702084385bb7e995b3344b2da
David Alsh <12656294+alshdavid@users.noreply.github.com>
2024-10-24 16:04:16 +0700

    V3: loadConfig for JS Plugins (#153)

--- packages/core/core/src/atlaspack-v3/worker/compat/plugin-config.js
+++ packages/core/core/src/atlaspack-v3/worker/compat/plugin-config.js
@@ -8,57 +8,78 @@ import type {
   FileCreateInvalidation,
   ConfigResultWithFilePath,
   PackageJSON,
+  PackageManager as IPackageManager,
 } from '@atlaspack/types';
+
+import type {FileSystem as IFileSystem} from '@atlaspack/fs';
+import ClassicPublicConfig from '../../../public/Config';
 
 export type PluginConfigOptions = {|
   isSource: boolean,
   searchPath: FilePath,
+  projectRoot: FilePath,
   env: Environment,
+  fs: IFileSystem,
+  packageManager: IPackageManager,
 |};
 
 export class PluginConfig implements IPluginConfig {
   isSource: boolean;
   searchPath: FilePath;
+  #projectRoot: FilePath;
   env: Environment;
+  #inner: ClassicPublicConfig;
 
-  constructor({env, isSource, searchPath}: PluginConfigOptions) {
+  constructor({
+    env,
+    isSource,
+    searchPath,
+    projectRoot,
+    fs,
+    packageManager,
+  }: PluginConfigOptions) {
     this.env = env;
     this.isSource = isSource;
     this.searchPath = searchPath;
+
+    this.#inner = new ClassicPublicConfig(
+      // $FlowFixMe
+      {
+        invalidateOnConfigKeyChange: [],
+        invalidateOnFileCreate: [],
+        invalidateOnFileChange: new Set(),
+        devDeps: [],
+        // $FlowFixMe
+        searchPath: searchPath.replace(projectRoot + '/', ''),
+      },
+      // $FlowFixMe
+      {
+        projectRoot,
+        inputFS: fs,
+        outputFS: fs,
+        packageManager,
+      },
+    );
   }
 
   // eslint-disable-next-line no-unused-vars
-  invalidateOnFileChange(filePath: FilePath): void {
-    throw new Error('PluginOptions.invalidateOnFileChange');
-  }
+  invalidateOnFileChange(filePath: FilePath): void {}
 
   // eslint-disable-next-line no-unused-vars
-  invalidateOnFileCreate(invalidations: FileCreateInvalidation): void {
-    throw new Error('PluginOptions.invalidateOnFileCreate');
-  }
+  invalidateOnFileCreate(invalidations: FileCreateInvalidation): void {}
 
   // eslint-disable-next-line no-unused-vars
-  invalidateOnEnvChange(invalidation: string): void {
-    throw new Error('PluginOptions.invalidateOnEnvChange');
-  }
+  invalidateOnEnvChange(invalidation: string): void {}
 
-  invalidateOnStartup(): void {
-    throw new Error('PluginOptions.invalidateOnStartup');
-  }
+  invalidateOnStartup(): void {}
 
-  invalidateOnBuild(): void {
-    throw new Error('PluginOptions.invalidateOnBuild');
-  }
+  invalidateOnBuild(): void {}
 
   // eslint-disable-next-line no-unused-vars
-  addDevDependency(options: DevDepOptions): void {
-    throw new Error('PluginOptions.addDevDependency');
-  }
+  addDevDependency(options: DevDepOptions): void {}
 
   // eslint-disable-next-line no-unused-vars
-  setCacheKey(key: string): void {
-    throw new Error('PluginOptions.setCacheKey');
-  }
+  setCacheKey(key: string): void {}
 
   getConfig<T>(
     // eslint-disable-next-line no-unused-vars
@@ -70,7 +91,7 @@ export class PluginConfig implements IPluginConfig {
       exclude?: boolean,
     |},
   ): Promise<?ConfigResultWithFilePath<T>> {
-    throw new Error('PluginOptions.getConfig');
+    return this.#inner.getConfig(filePaths, options);
   }
 
   getConfigFrom<T>(
@@ -85,10 +106,10 @@ export class PluginConfig implements IPluginConfig {
       exclude?: boolean,
     |},
   ): Promise<?ConfigResultWithFilePath<T>> {
-    throw new Error('PluginOptions.getConfigFrom');
+    return this.#inner.getConfigFrom(searchPath, filePaths, options);
   }
 
   getPackage(): Promise<?PackageJSON> {
-    throw new Error('PluginOptions.getPackage');
+    return this.#inner.getPackage();
   }
 }
