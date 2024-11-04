77cdbb3db559bc09792b92b383ccbe9103bc08b7
ngrujic <ngrujic@atlassian.com>
2024-11-05 09:45:16 +1100

    add async/await to namers

--- crates/atlaspack_core/src/plugin/namer_plugin.rs
+++ crates/atlaspack_core/src/plugin/namer_plugin.rs
@@ -1,20 +1,21 @@
-use std::fmt::Debug;
-use std::path::PathBuf;
-
 use crate::bundle_graph::BundleGraph;
 use crate::types::Bundle;
+use async_trait::async_trait;
+use std::fmt::Debug;
+use std::path::PathBuf;
 
 /// Determines the output filename for a bundle
 ///
 /// Namers run in a pipeline until one returns a result.
 ///
-pub trait NamerPlugin: Debug {
+#[async_trait]
+pub trait NamerPlugin: Debug + Send + Sync {
   /// Names the given bundle
   ///
   /// The returned file path should be relative to the target dist directory, and will be used to
   /// name the bundle. Naming can be forwarded onto the next plugin by returning None.
   ///
-  fn name(
+  async fn name(
     &self,
     bundle: &Bundle,
     bundle_graph: &BundleGraph,
@@ -28,8 +29,9 @@ mod tests {
   #[derive(Debug)]
   struct TestNamerPlugin {}
 
+  #[async_trait]
   impl NamerPlugin for TestNamerPlugin {
-    fn name(
+    async fn name(
       &self,
       _bundle: &Bundle,
       _bundle_graph: &BundleGraph,
