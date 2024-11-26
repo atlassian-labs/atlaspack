7b4773ed8c09db8444724b8b74a6ba57846a5efd
Pedro Yamada <tacla.yamada@gmail.com>
2024-11-27 10:06:08 +1100

    Remove dead code

--- packages/bundlers/bundler-experimental/src/DominatorBundler.js
+++ packages/bundlers/bundler-experimental/src/DominatorBundler.js
@@ -308,35 +308,3 @@ export function addNodeToBundle(
     }
   }, nodeId);
 }
-
-function getEntryByTarget(bundleGraph: MutableBundleGraph): {|
-  targets: DefaultMap<string, Map<Asset, Dependency>>,
-  allEntries: Map<Asset, Dependency>,
-|} {
-  // Find entries from assetGraph per target
-  let targets: DefaultMap<string, Map<Asset, Dependency>> = new DefaultMap(
-    () => new Map(),
-  );
-  const allEntries = new Map();
-  bundleGraph.traverse({
-    enter(node, context, actions) {
-      if (node.type !== 'asset') {
-        return node;
-      }
-      invariant(
-        context != null &&
-          context.type === 'dependency' &&
-          context.value.isEntry &&
-          context.value.target != null,
-      );
-
-      targets.get(context.value.target.distDir).set(node.value, context.value);
-      allEntries.set(node.value, context.value);
-
-      actions.skipChildren();
-      return node;
-    },
-  });
-
-  return {targets, allEntries};
-}
