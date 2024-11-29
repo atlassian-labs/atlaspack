148fea476ea4215949954a7d9267f6ad3cd6d549
mattcompiles <mattjones701@gmail.com>
2024-11-29 21:19:51 +1100

    Add V2/V3 output comparison tests

--- packages/core/integration-tests/test/atlaspack-v3.js
+++ packages/core/integration-tests/test/atlaspack-v3.js
@@ -1,5 +1,6 @@
 // @flow
 
+import assert from 'assert';
 import {join} from 'path';
 
 import {AtlaspackV3, FileSystemV3} from '@atlaspack/core';
@@ -10,8 +11,32 @@ import {
   inputFS,
   it,
   overlayFS,
+  outputFS,
+  bundle,
 } from '@atlaspack/test-utils';
 import {LMDBLiteCache} from '@atlaspack/cache';
+
+async function assertOutputIsIdentical(entry: string) {
+  let bundlesV3 = await bundle(entry, {
+    inputFS: overlayFS,
+  }).then((b) => b.getBundles());
+
+  let bundlesV2 = await bundle(entry, {
+    inputFS: overlayFS,
+    featureFlags: {
+      atlaspackV3: false,
+    },
+  }).then((b) => b.getBundles());
+
+  assert.equal(bundlesV3.length, bundlesV2.length);
+
+  for (let i = 0; i < bundlesV2.length; i++) {
+    let v2Code = await outputFS.readFile(bundlesV2[i].filePath, 'utf8');
+    let v3Code = await outputFS.readFile(bundlesV3[i].filePath, 'utf8');
+
+    assert.equal(v3Code, v2Code);
+  }
+}
 
 describe.v3('AtlaspackV3', function () {
   it('builds', async () => {
@@ -40,5 +65,52 @@ describe.v3('AtlaspackV3', function () {
     });
 
     await atlaspack.buildAssetGraph();
+  });
+
+  describe.only('should mirror V2 output', () => {
+    it('with dynamic resolver code', async () => {
+      await fsFixture(overlayFS, __dirname)`
+        resolver-code
+          index.js:
+            import theGlobal from 'theGlobal';
+            sideEffectNoop(theGlobal)
+          index.html:
+            <script type="module" src="./index.js" />
+          package.json:
+            {
+              "alias": {
+                "theGlobal": {
+                  "global": "MY_GLOBAL"
+                }
+              }
+            }
+          yarn.lock:
+      `;
+
+      await assertOutputIsIdentical(
+        join(__dirname, 'resolver-code/index.html'),
+      );
+    });
+
+    it('with CSS modules', async () => {
+      await fsFixture(overlayFS, __dirname)`
+        css-modules
+          css.module.css:
+            .composed {
+              background: pink;
+            }
+            .foo {
+              composes: composed;
+              color: white;
+            }
+          index.js:
+            import {foo} from './css.module.css';
+            sideEffectNoop(foo)
+          index.html:
+            <script type="module" src="./index.js" />
+      `;
+
+      await assertOutputIsIdentical(join(__dirname, 'css-modules/index.html'));
+    });
   });
 });
