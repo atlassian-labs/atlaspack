7ea5ae019aa0fb2709566e2905ccbc68841dd9a1
Pedro Yamada <tacla.yamada@gmail.com>
2024-12-02 11:20:59 +1100

    Add a few simple fast dominance tests

--- packages/bundlers/bundler-experimental/test/DominatorBundler/findAssetDominators.test.js
+++ packages/bundlers/bundler-experimental/test/DominatorBundler/findAssetDominators.test.js
@@ -20,20 +20,29 @@ describe('findAssetDominators', () => {
     await workerFarm.callAllWorkers('ping', []);
   });
 
+  const baseGraph = () => {
+    const inputGraph = new EdgeContentGraph();
+    const root = inputGraph.addNodeByContentKey('root', 'root');
+    inputGraph.setRootNodeId(root);
+    return {inputGraph, root};
+  };
+
   describe('findAssetDominators', () => {
     it('works on an empty graph', () => {
-      const inputGraph = new EdgeContentGraph();
-      const root = inputGraph.addNodeByContentKey('root', 'root');
-      inputGraph.setRootNodeId(root);
+      // digraph g {
+      //   root
+      // }
+      const {inputGraph, root} = baseGraph();
       const dominators = simpleFastDominance(inputGraph);
       const result = buildDominatorTree(inputGraph, dominators);
       assert.deepEqual(result.nodes, [result.getNodeByContentKey('root')]);
     });
 
     it('works on a single node graph', () => {
-      const inputGraph = new EdgeContentGraph();
-      const root = inputGraph.addNodeByContentKey('root', 'root');
-      inputGraph.setRootNodeId(root);
+      // digraph g {
+      //   root -> a
+      // }
+      const {inputGraph, root} = baseGraph();
       const a = inputGraph.addNodeByContentKey('a', 'a');
 
       inputGraph.addWeightedEdge(root, a, 1, 'weight');
@@ -61,22 +70,116 @@ describe('findAssetDominators', () => {
       );
     });
 
-    dotTest(__filename, 'can find dominators for a simple graph', async () => {
-      const entryPath = path.join(__dirname, 'test/test.js');
-      const entryDir = path.dirname(entryPath);
-      const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
-        asset('test.js', ['dependency.js', {to: 'async.js', type: 'async'}]),
-        asset('async.js', []),
-        asset('dependency.js', []),
-      ]);
+    it('it works on a linear graph', () => {
+      // digraph g {
+      //   root -> a -> b -> c -> d
+      // }
+      const {inputGraph, root} = baseGraph();
+      inputGraph.setRootNodeId(root);
 
-      const {mutableBundleGraph} = await setupBundlerTest(entryPath);
-      const {dominators} = findAssetDominators(mutableBundleGraph);
+      const a = inputGraph.addNodeByContentKey('a', 'a');
+      const b = inputGraph.addNodeByContentKey('b', 'b');
+      const c = inputGraph.addNodeByContentKey('c', 'c');
+      const d = inputGraph.addNodeByContentKey('d', 'd');
 
-      const outputDot = rootedGraphToDot(entryDir, dominators);
-      assert.equal(
-        outputDot,
-        `
+      inputGraph.addEdge(root, a);
+      inputGraph.addEdge(a, b);
+      inputGraph.addEdge(b, c);
+      inputGraph.addEdge(c, d);
+
+      const dominators = simpleFastDominance(inputGraph);
+      assert.equal(dominators[root], root);
+      assert.equal(dominators[a], root);
+      assert.equal(dominators[b], a);
+      assert.equal(dominators[c], b);
+      assert.equal(dominators[d], c);
+    });
+
+    it('it works on a tree graph', () => {
+      // digraph g {
+      //   root -> a;
+      //   root -> b;
+      //   a -> c;
+      //   a -> d;
+      //   b -> e;
+      // }
+      const {inputGraph, root} = baseGraph();
+
+      const a = inputGraph.addNodeByContentKey('a', 'a');
+      const b = inputGraph.addNodeByContentKey('b', 'b');
+      const c = inputGraph.addNodeByContentKey('c', 'c');
+      const d = inputGraph.addNodeByContentKey('d', 'd');
+      const e = inputGraph.addNodeByContentKey('e', 'e');
+
+      inputGraph.addEdge(root, a);
+      inputGraph.addEdge(root, b);
+      inputGraph.addEdge(a, c);
+      inputGraph.addEdge(a, d);
+      inputGraph.addEdge(b, e);
+
+      const dominators = simpleFastDominance(inputGraph);
+      assert.equal(dominators[root], root);
+      assert.equal(dominators[a], root);
+      assert.equal(dominators[b], root);
+      assert.equal(dominators[c], a);
+      assert.equal(dominators[d], a);
+      assert.equal(dominators[e], b);
+    });
+
+    it('it works on a graph with multiple paths to nodes', () => {
+      // digraph g {
+      //   root -> a;
+      //   root -> b;
+      //   a -> c;
+      //   a -> d;
+      //   b -> d;
+      //   d -> c;
+      // }
+      const {inputGraph, root} = baseGraph();
+
+      const a = inputGraph.addNodeByContentKey('a', 'a');
+      const b = inputGraph.addNodeByContentKey('b', 'b');
+      const c = inputGraph.addNodeByContentKey('c', 'c');
+      const d = inputGraph.addNodeByContentKey('d', 'd');
+
+      inputGraph.addEdge(root, a);
+      inputGraph.addEdge(root, b);
+      inputGraph.addEdge(a, c);
+      inputGraph.addEdge(a, d);
+      inputGraph.addEdge(b, d);
+      inputGraph.addEdge(d, c);
+
+      const dominators = simpleFastDominance(inputGraph);
+      assert.equal(dominators[root], root);
+      assert.equal(dominators[a], root);
+      assert.equal(dominators[b], root);
+      assert.equal(dominators[c], root);
+      assert.equal(dominators[d], root);
+    });
+
+    describe('fixture tests', () => {
+      dotTest(
+        __filename,
+        'can find dominators for a simple graph',
+        async () => {
+          const entryPath = path.join(__dirname, 'test/test.js');
+          const entryDir = path.dirname(entryPath);
+          const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
+            asset('test.js', [
+              'dependency.js',
+              {to: 'async.js', type: 'async'},
+            ]),
+            asset('async.js', []),
+            asset('dependency.js', []),
+          ]);
+
+          const {mutableBundleGraph} = await setupBundlerTest(entryPath);
+          const {dominators} = findAssetDominators(mutableBundleGraph);
+
+          const outputDot = rootedGraphToDot(entryDir, dominators);
+          assert.equal(
+            outputDot,
+            `
 digraph dominators {
   labelloc="t";
   label="Dominators";
@@ -91,37 +194,38 @@ digraph dominators {
   "test.js" -> "dependency.js";
 }
       `.trim(),
+          );
+
+          return [
+            {label: 'input', dot: inputDot},
+            {label: 'output', dot: outputDot},
+          ];
+        },
       );
 
-      return [
-        {label: 'input', dot: inputDot},
-        {label: 'output', dot: outputDot},
-      ];
-    });
+      dotTest(
+        __filename,
+        'can find dominators for a slightly more complex graph',
+        async () => {
+          const entryPath = path.join(__dirname, 'test/page.js');
+          const entryDir = path.dirname(entryPath);
+          const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
+            asset('page.js', ['react.js', 'lodash.js']),
+            asset('react.js', ['left-pad.js', 'string-concat.js', 'jsx.js']),
+            asset('lodash.js', ['left-pad.js']),
+            asset('left-pad.js', ['string-concat.js']),
+            asset('jsx.js', []),
+            asset('string-concat.js', ['string-chart-at.js']),
+            asset('string-chart-at.js', []),
+          ]);
 
-    dotTest(
-      __filename,
-      'can find dominators for a slightly more complex graph',
-      async () => {
-        const entryPath = path.join(__dirname, 'test/page.js');
-        const entryDir = path.dirname(entryPath);
-        const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
-          asset('page.js', ['react.js', 'lodash.js']),
-          asset('react.js', ['left-pad.js', 'string-concat.js', 'jsx.js']),
-          asset('lodash.js', ['left-pad.js']),
-          asset('left-pad.js', ['string-concat.js']),
-          asset('jsx.js', []),
-          asset('string-concat.js', ['string-chart-at.js']),
-          asset('string-chart-at.js', []),
-        ]);
+          const {mutableBundleGraph} = await setupBundlerTest(entryPath);
+          const {dominators} = findAssetDominators(mutableBundleGraph);
 
-        const {mutableBundleGraph} = await setupBundlerTest(entryPath);
-        const {dominators} = findAssetDominators(mutableBundleGraph);
-
-        const outputDot = rootedGraphToDot(entryDir, dominators);
-        assert.equal(
-          outputDot,
-          `
+          const outputDot = rootedGraphToDot(entryDir, dominators);
+          assert.equal(
+            outputDot,
+            `
 digraph dominators {
   labelloc="t";
   label="Dominators";
@@ -144,43 +248,43 @@ digraph dominators {
   "string-concat.js" -> "string-chart-at.js";
 }
             `.trim(),
-        );
+          );
 
-        return [
-          {label: 'input', dot: inputDot},
-          {label: 'output', dot: outputDot},
-        ];
-      },
-    );
+          return [
+            {label: 'input', dot: inputDot},
+            {label: 'output', dot: outputDot},
+          ];
+        },
+      );
 
-    dotTest(
-      __filename,
-      'works when there are multiple entry-points',
-      async () => {
-        const entryDir = path.join(__dirname, 'test');
-        const entryPath1 = path.join(entryDir, 'page1.js');
-        const entryPath2 = path.join(entryDir, 'page2.js');
-        const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
-          asset('page1.js', ['react.js', 'lodash.js']),
-          asset('page2.js', ['lodash.js', 'react.js']),
-          asset('react.js', ['left-pad.js', 'string-concat.js', 'jsx.js']),
-          asset('lodash.js', ['left-pad.js']),
-          asset('left-pad.js', ['string-concat.js']),
-          asset('jsx.js', []),
-          asset('string-concat.js', ['string-chart-at.js']),
-          asset('string-chart-at.js', []),
-        ]);
+      dotTest(
+        __filename,
+        'works when there are multiple entry-points',
+        async () => {
+          const entryDir = path.join(__dirname, 'test');
+          const entryPath1 = path.join(entryDir, 'page1.js');
+          const entryPath2 = path.join(entryDir, 'page2.js');
+          const inputDot = await fixtureFromGraph(entryDir, overlayFS, [
+            asset('page1.js', ['react.js', 'lodash.js']),
+            asset('page2.js', ['lodash.js', 'react.js']),
+            asset('react.js', ['left-pad.js', 'string-concat.js', 'jsx.js']),
+            asset('lodash.js', ['left-pad.js']),
+            asset('left-pad.js', ['string-concat.js']),
+            asset('jsx.js', []),
+            asset('string-concat.js', ['string-chart-at.js']),
+            asset('string-chart-at.js', []),
+          ]);
 
-        const {mutableBundleGraph} = await setupBundlerTest([
-          entryPath1,
-          entryPath2,
-        ]);
-        const {dominators} = findAssetDominators(mutableBundleGraph);
+          const {mutableBundleGraph} = await setupBundlerTest([
+            entryPath1,
+            entryPath2,
+          ]);
+          const {dominators} = findAssetDominators(mutableBundleGraph);
 
-        const outputDot = rootedGraphToDot(entryDir, dominators);
-        assert.equal(
-          outputDot,
-          `
+          const outputDot = rootedGraphToDot(entryDir, dominators);
+          assert.equal(
+            outputDot,
+            `
 digraph dominators {
   labelloc="t";
   label="Dominators";
@@ -205,13 +309,14 @@ digraph dominators {
   "string-concat.js" -> "string-chart-at.js";
 }
             `.trim(),
-        );
+          );
 
-        return [
-          {label: 'input', dot: inputDot},
-          {label: 'output', dot: outputDot},
-        ];
-      },
-    );
+          return [
+            {label: 'input', dot: inputDot},
+            {label: 'output', dot: outputDot},
+          ];
+        },
+      );
+    });
   });
 });
