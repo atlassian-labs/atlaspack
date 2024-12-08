d1872e8e635b88dd389b1052188edbff8cfade7e
Ben Jervis <ben@jervis.net.au>
2024-12-09 10:42:58 +1100

    Add some tests

--- packages/runtimes/js/test/bundle-url-shards.test.js
+++ packages/runtimes/js/test/bundle-url-shards.test.js
@@ -1,3 +1,5 @@
+import assert from 'assert';
+
 import {
   getShardedBundleURL,
   getBaseURL,
@@ -5,57 +7,87 @@ import {
 
 const testingCookieName = 'DOMAIN_SHARDING_TEST';
 
-describe('getShardedBundleURL', () => {
-  it('should shard a URL if the cookie is present', () => {
-    const testBundle = 'TestBundle.123abc.js';
+const createErrorStack = (url) => {
+  // This error stack is copied from a local dev
+  return `
+Error
+    at getShardedBundleURL (http://localhost:8081/main-entry-bundle.1a2fa8b7.js:13663:29)
+    at Object.getShardedBundleURLCached [as getShardedBundleURL] (http://localhost:8081/main-entry-bundle.1a2fa8b7.js:13639:17)
+    at jrxWb.d7fe7af8a40b5d68 (${url}?1733635254967:695:74)
+    at newRequire (http://localhost-4:8081/Heartbeat.bc74c821.js?1733635254967:71:24)
+    at localRequire (http://localhost-4:8081/Heartbeat.bc74c821.js?1733635254967:84:35)
+    at loader (http://localhost-4:8081/Heartbeat.bc74c821.js?1733635254967:691:29)
+    at runLoader (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:56535:20)
+    at Object.run (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:56211:28)
+    at TaskManager.<anonymous> (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:56132:33)
+    at invokeFunc (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:53665:23)
+    at trailingEdge (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:53697:42)
+    at timerExpired (http://localhost:8081/dashboard-spa-container-jquery3.40388a2a.js:53689:40)
+`.trim();
+};
 
-    // TODO: mock the error stacktrace somehow
+describe.only('bundle-url-shards helper', () => {
+  describe('getShardedBundleURL', () => {
+    it('should shard a URL if the cookie is present', () => {
+      const testBundle = 'test-bundle.123abc.js';
 
-    const result = getShardedBundleURL(
-      testBundle,
-      testingCookieName,
-      `${testingCookieName}=1`,
-      5,
-    );
+      const err = new Error();
+      err.stack = createErrorStack(
+        'https://bundle-shard.assets.example.com/assets/ParentBundle.cba321.js',
+      );
 
-    expect(result).toBe('https://bundle-shard-3.assets.example.com');
+      const result = getShardedBundleURL(
+        testBundle,
+        testingCookieName,
+        `${testingCookieName}=1`,
+        5,
+        err,
+      );
+
+      assert.equal(result, 'https://bundle-shard-0.assets.example.com/assets/');
+    });
+
+    it('should re-shard a domain that has already been sharded', () => {
+      const testBundle = 'TestBundle.1a2b3c.js';
+
+      const err = new Error();
+      err.stack = createErrorStack(
+        'https://bundle-shard-1.assets.example.com/assets/ParentBundle.cba321.js',
+      );
+
+      const result = getShardedBundleURL(
+        testBundle,
+        testingCookieName,
+        `${testingCookieName}=1`,
+        5,
+        err,
+      );
+
+      assert.equal(result, 'https://bundle-shard-4.assets.example.com/assets/');
+    });
   });
 
-  it('should re-shard a domain that has already been sharded', () => {
-    const testBundle = 'TestBundle.123abc.js';
+  describe('getBaseUrl', () => {
+    it('should return the URL with the filename removed', () => {
+      const testUrl =
+        'https://bundle-shard-3.assets.example.com/assets/testBundle.123abc.js';
 
-    // TODO: mock the error stacktrace somehow
+      assert.equal(
+        getBaseURL(testUrl),
+        'https://bundle-shard-3.assets.example.com/assets/',
+      );
+    });
 
-    const result = getShardedBundleURL(
-      testBundle,
-      testingCookieName,
-      `${testingCookieName}=1`,
-      5,
-    );
+    it('should handle domains with no .', () => {
+      const testUrl = 'http://localhost/assets/testBundle.123abc.js';
 
-    expect(result).toBe('https://bundle-shard-3.assets.example.com');
-  });
-});
+      assert.equal(getBaseURL(testUrl), 'http://localhost-1/assets/');
+    });
 
-describe('getBaseUrl', () => {
-  it('should return the URL with the filename removed', () => {
-    const testUrl =
-      'https://bundle-shard-3.assets.example.com/assets/testBundle.123abc.js';
+    it('should handle domains with ports', () => {
+      const testUrl = 'http://localhost:8081/assets/testBundle.123abc.js';
 
-    expect(getBaseURL(testUrl)).toBe(
-      'https://bundle-shard-3.assets.example.com/assets/',
-    );
-  });
-
-  it('should handle domains with no .', () => {
-    const testUrl = 'http://localhost/assets/testBundle.123abc.js';
-
-    expect(getBaseURL(testUrl)).toBe('http://localhost/assets/');
-  });
-
-  it('should handle domains with ports', () => {
-    const testUrl = 'http://localhost:8081/assets/testBundle.123abc.js';
-
-    expect(getBaseURL(testUrl)).toBe('http://localhost:8081/assets/');
+      assert.equal(getBaseURL(testUrl), 'http://localhost-1:8081/assets/');
+    });
   });
 });
