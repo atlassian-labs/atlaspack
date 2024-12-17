60c02dd9d77e399967b9b4d41ec5b80f4471ac0e
Ben Jervis <ben@jervis.net.au>
2024-12-17 13:33:10 +1100

    Add unit test to bundle-url

--- packages/runtimes/js/test/bundle-url.test.js
+++ packages/runtimes/js/test/bundle-url.test.js
@@ -2,15 +2,16 @@
 import assert from 'assert';
 import {fsFixture, overlayFS, bundle} from '@atlaspack/test-utils';
 
-import {getBaseURL} from '../src/helpers/bundle-url';
+import {getBaseURL, getBundleURL} from '../src/helpers/bundle-url';
 
 const createErrorStack = (url) => {
   // This error stack is copied from a local dev, with a bunch
   // of lines trimmed off the end so it's not unnecessarily long
   return `
 Error
-    at Object.getShardedBundleURL (http://localhost:8081/main-bundle.1a2fa8b7.js:15688:29)
-    at a7u9v.6d3ceb6ac67fea50 (${url}.1a2fa8b7.js:361466:46)
+    at Object.getBundleURL (http://localhost:8081/main-bundle.1a2fa8b7.js:15688:29)
+    at Object.getBundleURLCached (http://localhost:8081/main-bundle.1a2fa8b7.js:15688:29)
+    at a7u9v.6d3ceb6ac67fea50 (${url}:361466:46)
     at newRequire (http://localhost:8081/main-bundle.1a2fa8b7.js:71:24)
     at localRequire (http://localhost:8081/main-bundle.1a2fa8b7.js:84:35)
     at 7H8wc.react-intl-next (http://localhost:8081/main-bundle.1a2fa8b7.js:279746:28)
@@ -43,5 +44,20 @@ describe('getBaseUrl', () => {
     const testUrl = 'http://localhost:8081/assets/testBundle.123abc.js';
 
     assert.equal(getBaseURL(testUrl), 'http://localhost:8081/assets/');
+  });
+});
+
+describe('getBundleURL', () => {
+  it('should return the URL of the bundle without the file name', () => {
+    const testUrl =
+      'https://bundle-domain.assets.example.com/assets/testBundle.123abc.js';
+
+    const errorStack = createErrorStack(testUrl);
+    const error = new Error();
+    error.stack = errorStack;
+
+    const result = getBundleURL('test-asset', error);
+
+    assert.equal(result, 'https://bundle-domain.assets.example.com/assets/');
   });
 });
