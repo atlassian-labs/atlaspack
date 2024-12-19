c6c15ce1da779c34e2d7c7c3dda04b32b1614267
Ben Jervis <ben@jervis.net.au>
2024-12-19 11:25:47 +1100

    Add an integration test for the ESM case

--- packages/core/integration-tests/test/domain-sharding.js
+++ packages/core/integration-tests/test/domain-sharding.js
@@ -2,7 +2,8 @@
 import assert from 'assert';
 import path from 'path';
 
-import {fsFixture, overlayFS, bundle} from '@atlaspack/test-utils';
+import {fsFixture, overlayFS, bundle, run} from '@atlaspack/test-utils';
+import {domainShardingKey} from '@atlaspack/domain-sharding';
 
 const maxShards = 8;
 
@@ -119,6 +120,71 @@ describe('domain-sharding', () => {
         ),
         'Expected generated code for shardUrl was not found',
       );
+    });
+
+    it('for ESM loaded bundle manifest', async () => {
+      const maxShards = 8;
+      await fsFixture(overlayFS)`
+        package.json:
+          {
+            "name": "bundle-sharding-test",
+            "@atlaspack/runtime-js": {
+              "domainSharding": {
+                "maxShards": ${maxShards}
+              }
+            }
+          }
+
+        src/index.js:
+          async function fn() {
+            const a = await import('./a.js');
+            const b = await import('./b.js');
+            console.log('a', a, b);
+          }
+          fn();
+
+        src/a.js:
+          export const a = async () => {
+            const b = await import('./b');
+            return b + 'A';
+          }
+        src/b.js:
+          export const b = 'B';
+
+        yarn.lock:
+      `;
+
+      const bundleGraph = await bundle('src/index.js', {
+        inputFS: overlayFS,
+        mode: 'production',
+        defaultTargetOptions: {
+          shouldOptimize: false,
+          outputFormat: 'esmodule',
+        },
+      });
+
+      const mainBundle = bundleGraph
+        .getBundles()
+        .find((b) => b.name === 'index.js');
+
+      if (!mainBundle) {
+        return assert(mainBundle);
+      }
+
+      // $FlowFixMe - Flow doesn't seem to know about doesNotReject
+      await assert.doesNotReject(
+        run(bundleGraph, {[domainShardingKey]: true}),
+        'Expected bundle to be able to execute',
+      );
+
+      const code = await overlayFS.readFile(mainBundle.filePath, 'utf-8');
+
+      const expectedCode = `var$load = (parcelRequire("cfSxn"))(${maxShards});`
+        .replaceAll('$', '\\$')
+        .replaceAll('(', '\\(')
+        .replaceAll(')', '\\)');
+
+      assert.match(code, new RegExp(expectedCode));
     });
   });
 });
