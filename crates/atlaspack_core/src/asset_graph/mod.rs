4cc252099f2ea29bd8c3ab7575c08532270074e4
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-01-23 07:14:15 +0700

    V3: Using JsBuffer to transfer AssetGraph to Nodejs (#313)

--- crates/atlaspack_core/src/asset_graph/mod.rs
+++ crates/atlaspack_core/src/asset_graph/mod.rs
@@ -1,8 +1,6 @@
 #[allow(clippy::module_inception)]
 mod asset_graph;
 mod propagate_requested_symbols;
-mod serialize_asset_graph;
 
 pub use self::asset_graph::*;
 pub use self::propagate_requested_symbols::*;
-pub use self::serialize_asset_graph::*;
