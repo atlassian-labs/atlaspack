2fea5f2c1ab381008f21fe960c9103b8b692c8ef
Matt Jones <mattjones701@gmail.com>
2025-01-24 14:07:10 +1100

    Add base domain to domain sharding possibilities (#314)

--- packages/utils/domain-sharding/test/index.test.js
+++ packages/utils/domain-sharding/test/index.test.js
@@ -1,11 +1,54 @@
 // @flow
 import assert from 'assert';
-import {shardUrlUnchecked, shardUrl, domainShardingKey} from '../src/index.js';
+import {
+  shardUrlUnchecked,
+  shardUrl,
+  domainShardingKey,
+  applyShardToDomain,
+} from '../src/index.js';
 
 describe('domain sharding helpers', () => {
   beforeEach(() => {
     // $FlowFixMe
     delete globalThis[domainShardingKey];
+  });
+
+  describe('applyShardToDomain', () => {
+    it('should use the base domain for shard 0', () => {
+      const testBundle =
+        'https://bundle-shard.assets.example.com/assets/test-bundle.123abc.js';
+
+      const result = applyShardToDomain(testBundle, 0);
+
+      assert.equal(
+        result,
+        'https://bundle-shard.assets.example.com/assets/test-bundle.123abc.js',
+      );
+    });
+
+    it('should use the 0 shard domain for shard 1', () => {
+      const testBundle =
+        'https://bundle-shard.assets.example.com/assets/test-bundle.123abc.js';
+
+      const result = applyShardToDomain(testBundle, 1);
+
+      assert.equal(
+        result,
+        'https://bundle-shard-0.assets.example.com/assets/test-bundle.123abc.js',
+      );
+    });
+
+    it('should use the 2 shard domain for shard 3', () => {
+      const testBundle =
+        'https://bundle-shard.assets.example.com/assets/test-bundle.123abc.js';
+
+      const result = applyShardToDomain(testBundle, 3);
+
+      assert.equal(
+        result,
+        'https://bundle-shard-2.assets.example.com/assets/test-bundle.123abc.js',
+      );
+    });
   });
 
   describe('shardUrlUnchecked', () => {
@@ -17,7 +60,19 @@ describe('domain sharding helpers', () => {
 
       assert.equal(
         result,
-        'https://bundle-shard-0.assets.example.com/assets/test-bundle.123abc.js',
+        'https://bundle-shard-1.assets.example.com/assets/test-bundle.123abc.js',
+      );
+    });
+
+    it('should include the original domain in the shar', () => {
+      const testBundle =
+        'https://bundle-shard.assets.example.com/assets/test-bundle.123abc.js';
+
+      const result = shardUrlUnchecked(testBundle, 5);
+
+      assert.equal(
+        result,
+        'https://bundle-shard-1.assets.example.com/assets/test-bundle.123abc.js',
       );
     });
 
@@ -29,7 +84,7 @@ describe('domain sharding helpers', () => {
 
       assert.equal(
         result,
-        'https://bundle-shard-4.assets.example.com/assets/TestBundle.1a2b3c.js',
+        'https://bundle-shard-2.assets.example.com/assets/TestBundle.1a2b3c.js',
       );
     });
 
@@ -38,7 +93,7 @@ describe('domain sharding helpers', () => {
 
       const result = shardUrlUnchecked(testBundle, 5);
 
-      assert.equal(result, 'http://localhost-3/assets/testBundle.123abc.js');
+      assert.equal(result, 'http://localhost-2/assets/testBundle.123abc.js');
     });
 
     it('should handle domains with ports', () => {
@@ -48,7 +103,7 @@ describe('domain sharding helpers', () => {
 
       assert.equal(
         result,
-        'http://localhost-3:8081/assets/testBundle.123abc.js',
+        'http://localhost-2:8081/assets/testBundle.123abc.js',
       );
     });
   });
@@ -63,7 +118,7 @@ describe('domain sharding helpers', () => {
 
       assert.equal(
         result,
-        'https://bundle-shard-0.assets.example.com/assets/test-bundle.123abc.js',
+        'https://bundle-shard-1.assets.example.com/assets/test-bundle.123abc.js',
       );
     });
 
