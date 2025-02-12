64cd5e9ae573dbf0bee64488c7cf2026f991a1ed
Pedro Tacla Yamada <tacla.yamada@gmail.com>
2025-02-12 14:59:57 +1100

    Add 'IdentifierRegistry' mechanism for debugging IDs
    Whenever an ID is generated, we will optionally log it to a file with the data
    used to generate it.
    
    This will help us debug generated identifiers on artefacts or runtime errors.
    
    Test Plan: N/A
    
    Reviewers: MonicaOlejniczak
    
    Reviewed By: MonicaOlejniczak
    
    Pull Request: https://github.com/atlassian-labs/atlaspack/pull/333

---
+++ packages/core/core/src/IdentifierRegistry.js
@@ -0,0 +1,32 @@
+// @flow strict-local
+
+import fs from 'fs';
+
+export class IdentifierRegistry {
+  #enabled: boolean;
+  #createdDirectory: boolean = false;
+
+  constructor(enabled: boolean) {
+    this.#enabled = enabled;
+  }
+
+  addIdentifier(type: string, identifier: string, data: mixed) {
+    if (this.#enabled) {
+      if (!this.#createdDirectory) {
+        fs.mkdirSync('./.atlaspack', {recursive: true});
+        this.#createdDirectory = true;
+      }
+
+      fs.appendFile(
+        './.atlaspack/atlaspack-identifiers.txt',
+        // $FlowFixMe
+        `${type} ${identifier} ${JSON.stringify(data)}\n`,
+        () => {},
+      );
+    }
+  }
+}
+
+export const identifierRegistry: IdentifierRegistry = new IdentifierRegistry(
+  process.env.ATLASPACK_IDENTIFIER_DEBUG === 'true',
+);
