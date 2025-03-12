7ef6c41ac16720c5667f598ad8930fbe69ffa635
Jake Lane <jlane2@atlassian.com>
2025-03-12 11:35:34 +1100

    Use new server syntax that works in jest env (#369)

--- packages/utils/babel-plugin-transform-contextual-imports/src/index.ts
+++ packages/utils/babel-plugin-transform-contextual-imports/src/index.ts
@@ -1,27 +1,59 @@
-import {PluginObj} from '@babel/core';
+import type {PluginObj, NodePath, types as BabelTypes} from '@babel/core';
 import {declare} from '@babel/helper-plugin-utils';
-import type {StringLiteral} from '@babel/types';
 
 interface Opts {
-  server?: boolean;
+  // Use node safe import cond syntax
+  node?: boolean;
 }
 
 interface State {
   opts: Opts;
-  importNodes?: any[]; // Statement types didn't work so using any
+  // Set of identifier names that need to be mutated after import was transformed
+  conditionalImportIdentifiers?: Set<string>;
+  // Set of identifiers that have been visited in the exit pass, to avoid adding the load property multiple times
+  visitedIdentifiers?: Set<BabelTypes.Identifier>;
 }
 
-const isServer = (opts: Opts) => {
-  return 'server' in opts && opts.server;
-};
+const isNode = (opts: Opts): boolean => !!('node' in opts && opts.node);
 
 export default declare((api): PluginObj<State> => {
   const {types: t} = api;
 
+  const isImportCondCallExpression = (
+    node: BabelTypes.Node,
+  ): node is BabelTypes.CallExpression & {
+    arguments: [
+      BabelTypes.StringLiteral,
+      BabelTypes.StringLiteral,
+      BabelTypes.StringLiteral,
+    ];
+  } => {
+    if (
+      node.type === 'CallExpression' &&
+      node.callee.type === 'Identifier' &&
+      node.callee.name === 'importCond'
+    ) {
+      if (
+        node.arguments.length === 3 &&
+        node.arguments.every(
+          (arg): arg is BabelTypes.StringLiteral =>
+            arg.type === 'StringLiteral',
+        )
+      ) {
+        return true;
+      } else {
+        // Simple error for incorrect syntax (since it's documented with the type)
+        throw new Error('importCond must have three string literal arguments');
+      }
+    }
+
+    return false;
+  };
+
   const buildCondFunction = (
-    cond: StringLiteral,
-    ifTrue: StringLiteral,
-    ifFalse: StringLiteral,
+    cond: BabelTypes.StringLiteral,
+    ifTrue: BabelTypes.StringLiteral,
+    ifFalse: BabelTypes.StringLiteral,
   ) =>
     t.conditionalExpression(
       t.logicalExpression(
@@ -45,16 +77,16 @@ export default declare((api): PluginObj<State> => {
       ),
     );
 
-  const buildServerObject = (
-    identUid: string,
-    cond: StringLiteral,
-    ifTrue: StringLiteral,
-    ifFalse: StringLiteral,
+  const buildNodeObject = (
+    identifier: BabelTypes.Identifier,
+    cond: BabelTypes.StringLiteral,
+    ifTrue: BabelTypes.StringLiteral,
+    ifFalse: BabelTypes.StringLiteral,
   ) => [
     // Create object containing imports
     t.variableDeclaration('const', [
       t.variableDeclarator(
-        t.identifier(identUid),
+        identifier,
         t.objectExpression([
           t.objectProperty(
             t.identifier('ifTrue'),
@@ -74,7 +106,8 @@ export default declare((api): PluginObj<State> => {
       ),
     ]),
 
-    // Create lazy getter via the load property on the object
+    // Create lazy getter via the load property on the object.
+    // This is node module resolution safe because each time the import is accessed, we re-evaluate the condition.
     t.expressionStatement(
       t.callExpression(
         t.memberExpression(
@@ -82,7 +115,7 @@ export default declare((api): PluginObj<State> => {
           t.identifier('defineProperty'),
         ),
         [
-          t.identifier(identUid),
+          identifier,
           t.stringLiteral('load'),
           t.objectExpression([
             t.objectProperty(
@@ -104,14 +137,8 @@ export default declare((api): PluginObj<State> => {
                       [cond],
                     ),
                   ),
-                  t.memberExpression(
-                    t.identifier(identUid),
-                    t.identifier('ifTrue'),
-                  ),
-                  t.memberExpression(
-                    t.identifier(identUid),
-                    t.identifier('ifFalse'),
-                  ),
+                  t.memberExpression(identifier, t.identifier('ifTrue')),
+                  t.memberExpression(identifier, t.identifier('ifFalse')),
                 ),
               ),
             ),
@@ -126,52 +153,64 @@ export default declare((api): PluginObj<State> => {
     visitor: {
       CallExpression: {
         enter(path, state) {
-          if (
-            path.node.callee.type === 'Identifier' &&
-            path.node.callee.name === 'importCond'
-          ) {
-            if (
-              path.node.arguments.length == 3 &&
-              path.node.arguments.every((arg) => arg.type === 'StringLiteral')
-            ) {
-              const [cond, ifTrue, ifFalse] = path.node.arguments;
-
-              if (isServer(state.opts)) {
-                // Make module pass lazy in ssr
-                const identUid = path.scope.generateUid(
-                  `${cond.value}$${ifTrue.value}$${ifFalse.value}`,
-                );
-
-                state.importNodes ??= [];
-                state.importNodes.push(
-                  ...buildServerObject(identUid, cond, ifTrue, ifFalse),
-                );
-
-                // Replace call expression with reference to lazy object getter
-                path.replaceWith(
-                  t.memberExpression(
-                    t.identifier(identUid),
-                    t.identifier('load'),
-                  ),
-                );
-              } else {
-                path.replaceWith(buildCondFunction(cond, ifTrue, ifFalse));
-              }
-            } else {
-              // Simple error for incorrect syntax (since it's documented with the type)
-              throw new Error(
-                'importCond must have three string literal arguments',
-              );
+          const node = path.node;
+          if (isImportCondCallExpression(node)) {
+            const [cond, ifTrue, ifFalse] = node.arguments;
+            if (!isNode(state.opts)) {
+              // Replace the importCond call with a conditional require import, as a fallback for environments that don't support Atlaspack
+              path.replaceWith(buildCondFunction(cond, ifTrue, ifFalse));
             }
           }
         },
       },
-      Program: {
-        exit(path, state) {
-          if (state.importNodes) {
-            // If there's an import
-            path.unshiftContainer('body', state.importNodes);
+      VariableDeclaration: {
+        enter(path, state) {
+          if (isNode(state.opts)) {
+            if (
+              path.node.declarations.length === 1 &&
+              path.node.declarations[0].type === 'VariableDeclarator' &&
+              path.node.declarations[0].id.type === 'Identifier'
+            ) {
+              const importId = path.node.declarations[0].id;
+              const call = path.node.declarations[0].init;
+
+              // Mark identifier for object so we don't add the load property to it
+              state.visitedIdentifiers?.add(importId);
+
+              if (call && isImportCondCallExpression(call)) {
+                const [cond, ifTrue, ifFalse] = call.arguments;
+
+                // Replace with object containing imports and lazy getter, which allows us to load the correct import based on the condition at runtime
+                path.replaceWithMultiple(
+                  buildNodeObject(importId, cond, ifTrue, ifFalse),
+                );
+
+                // Add identifier name to set so we can mutate all import usages in the exit pass
+                state.conditionalImportIdentifiers?.add(importId.name);
+              }
+            }
           }
+        },
+      },
+      Identifier: {
+        exit(path, state) {
+          const identifier = state.conditionalImportIdentifiers?.has(
+            path.node.name,
+          );
+          if (identifier && !state.visitedIdentifiers?.has(path.node)) {
+            // Add load property to the import usage
+            const newIdentifer = t.identifier(path.node.name);
+            path.replaceWith(
+              t.memberExpression(newIdentifer, t.identifier('load')),
+            );
+            state.visitedIdentifiers?.add(newIdentifer);
+          }
+        },
+      },
+      Program: {
+        enter(_, state) {
+          state.conditionalImportIdentifiers = new Set();
+          state.visitedIdentifiers = new Set();
         },
       },
     },
--- packages/utils/babel-plugin-transform-contextual-imports/test/babel-plugin-transform-contextual-imports.test.js
+++ packages/utils/babel-plugin-transform-contextual-imports/test/babel-plugin-transform-contextual-imports.test.js
@@ -8,40 +8,42 @@ const plugin = require.resolve('../src/index.ts');
 describe('@atlaspack/babel-plugin-transform-contextual-imports', () => {
   it('should transform importCond to inline requires', () => {
     const input = `
-      importCond('CONDITION', 'IF_TRUE', 'IF_FALSE');
+      const Imported = importCond('CONDITION', 'IF_TRUE', 'IF_FALSE');
     `;
-    let {code: transformed} = babel.transformSync(input, {
+    const {code: transformed} = babel.transformSync(input, {
       configFile: false,
       presets: [],
       plugins: [plugin],
     });
 
-    assert(
-      transformed ===
-        "globalThis.__MCOND && globalThis.__MCOND('CONDITION') ? require('IF_TRUE').default : require('IF_FALSE').default;",
+    assert.equal(
+      transformed,
+      "const Imported = globalThis.__MCOND && globalThis.__MCOND('CONDITION') ? require('IF_TRUE').default : require('IF_FALSE').default;",
     );
   });
 
   it('should transform importCond to ssr safe code', () => {
     const input = `
-      importCond('CONDITION', 'IF_TRUE', 'IF_FALSE');
+      const Imported = importCond('CONDITION', 'IF_TRUE', 'IF_FALSE');
+
+      console.log(Imported, Imported.someProperty);
     `;
-    let {code: transformed} = babel.transformSync(input, {
+    const {code: transformed} = babel.transformSync(input, {
       configFile: false,
       presets: [],
-      plugins: [[plugin, {server: true}]],
+      plugins: [[plugin, {node: true}]],
     });
 
-    assert(
-      transformed ===
-        `const _CONDITION$IF_TRUE$IF_FALSE = {
+    assert.equal(
+      transformed,
+      `const Imported = {
   ifTrue: require('IF_TRUE').default,
   ifFalse: require('IF_FALSE').default
 };
-Object.defineProperty(_CONDITION$IF_TRUE$IF_FALSE, "load", {
-  get: () => globalThis.__MCOND && globalThis.__MCOND('CONDITION') ? _CONDITION$IF_TRUE$IF_FALSE.ifTrue : _CONDITION$IF_TRUE$IF_FALSE.ifFalse
+Object.defineProperty(Imported, "load", {
+  get: () => globalThis.__MCOND && globalThis.__MCOND('CONDITION') ? Imported.ifTrue : Imported.ifFalse
 });
-_CONDITION$IF_TRUE$IF_FALSE.load;`,
+console.log(Imported.load, Imported.load.someProperty);`,
     );
   });
 });
