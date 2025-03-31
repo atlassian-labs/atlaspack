3205de69b8c464ea765f7456a7d15788a7d54e70
David Alsh <alshdavid@gmail.com>
2025-03-31 14:43:44 +1100

    update

--- crates/node-bindings/build.rs
+++ crates/node-bindings/build.rs
@@ -1,8 +1,6 @@
-#[cfg(not(target_arch = "wasm32"))]
 extern crate napi_build;
 
 fn main() {
-  #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
   println!("cargo:rerun-if-changed=build.rs");
 }
