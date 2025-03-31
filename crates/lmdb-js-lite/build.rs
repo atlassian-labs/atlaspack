01d276b7f564670a91626f93b2cf7045cbb49b27
David Alsh <alshdavid@gmail.com>
2025-03-31 14:41:47 +1100

    update

--- crates/lmdb-js-lite/build.rs
+++ crates/lmdb-js-lite/build.rs
@@ -2,4 +2,5 @@ extern crate napi_build;
 
 fn main() {
   napi_build::setup();
+  println!("cargo:rerun-if-changed=build.rs");
 }
--- crates/node-bindings/build.rs
+++ crates/node-bindings/build.rs
@@ -4,4 +4,5 @@ extern crate napi_build;
 fn main() {
   #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
+  println!("cargo:rerun-if-changed=build.rs");
 }
