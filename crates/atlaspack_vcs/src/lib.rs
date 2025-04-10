23117b1a69d9e71a611b54a00064ceb0dec947fe
David Alsh <alshdavid@gmail.com>
2025-04-10 15:11:00 +1000

    Added comments to the external fixutres

--- crates/atlaspack_vcs/src/lib.rs
+++ crates/atlaspack_vcs/src/lib.rs
@@ -263,7 +263,7 @@ pub struct VCSFile {
 
 fn get_file_contents_at_commit(
   repo: &Repository,
-  commit: &git2::Commit<'_>,
+  commit: &git2::Commit,
   path: &Path,
 ) -> anyhow::Result<Option<String>> {
   let tree = commit.tree()?;
--- crates/json-comments-rs/src/lib.rs
+++ crates/json-comments-rs/src/lib.rs
@@ -141,7 +141,7 @@ where
 
 fn consume_comment_whitespace_until_maybe_bracket(
   state: &mut State,
-  it: &mut IterMut<'_, u8>,
+  it: &mut IterMut<u8>,
   settings: &CommentSettings,
 ) -> Result<bool> {
   for c in it.by_ref() {
@@ -423,6 +423,8 @@ mod tests {
 
   #[test]
   fn line_comments() {
+    // Put these in an external file because VSCode automatically trims whitespace
+    // which is important to the tests. Removing the whitespace breaks the tests
     let json = include_str!("../fixtures/line_comments.txt").to_string();
     let expected = include_str!("../fixtures/line_comments_expected.txt").to_string();
     assert_eq!(strip_string(&json), expected);
@@ -506,6 +508,8 @@ mod tests {
 
   #[test]
   fn trailing_comma() {
+    // Put these in an external file because VSCode automatically trims whitespace
+    // which is important to the tests. Removing the whitespace breaks the tests
     let mut json = include_str!("../fixtures/trailing_comma.txt").to_string();
 
     #[allow(deprecated)]
