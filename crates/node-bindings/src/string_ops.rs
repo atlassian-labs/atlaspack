5172bfda2f17522c8d0b3ec8c83ffc1c24b439cc
Ben Jervis <ben@jervis.net.au>
2025-04-10 14:47:13 +1000

    Add aho-corasick

--- crates/node-bindings/src/string_ops.rs
+++ crates/node-bindings/src/string_ops.rs
@@ -1,3 +1,4 @@
+use aho_corasick::{AhoCorasick, MatchKind};
 use napi_derive::napi;
 
 #[napi(object)]
@@ -8,5 +9,18 @@ pub struct Replacement {
 
 #[napi]
 pub fn perform_string_replacements(input: String, replacements: Vec<Replacement>) -> String {
-  todo!("Write the function")
+  let mut froms = Vec::new();
+  let mut tos = Vec::new();
+
+  for replacement in &replacements {
+    froms.push(replacement.from.as_str());
+    tos.push(replacement.to.as_str());
+  }
+
+  let ac = AhoCorasick::builder()
+    .match_kind(MatchKind::Standard)
+    .build(froms)
+    .unwrap();
+
+  ac.replace_all(&input, &tos)
 }
