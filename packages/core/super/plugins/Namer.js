067a72b1621ef3c26d73b6e6f5305fc91e6f77b5
Matt Jones <mjones4@atlassian.com>
2025-04-23 09:38:53 +1000

    Fix workers with new namer

--- packages/core/super/plugins/Namer.js
+++ packages/core/super/plugins/Namer.js
@@ -1,16 +1,32 @@
 let path = require('path');
+let assert = require('assert');
 let {Namer} = require('@atlaspack/plugin');
+
+let workerEntries = {
+  // Use the hash reference for the oringal worker file
+  'core/workers/src/threads/ThreadsChild.js': (hashReference) =>
+    `core/workers/threads/ThreadsChild.${hashReference}.js`,
+  'core/workers/src/process/ProcessChild.js': (hashReference) =>
+    `core/workers/process/ProcessChild.${hashReference}.js`,
+  'core/core/src/worker.js': (hashReference) =>
+    `core/core/worker.${hashReference}.js`,
+  // Use a stable name for the entry worker files
+  'core/super/entries/ThreadsChild.js': () =>
+    `core/workers/threads/ThreadsChild.js`,
+  'core/super/entries/ProcessChild.js': () =>
+    `core/workers/process/ProcessChild.js`,
+  'core/super/entries/worker.js': () => `core/core/worker.js`,
+};
 
 module.exports = new Namer({
   name({bundle, options}) {
     let entryAsset = bundle.getMainEntry();
 
-    if (bundle.needsStableName) {
-      // This is an entry so just use it's base file name
-      return path.basename(entryAsset.filePath);
-    }
-
     if (entryAsset?.filePath.includes('/node_modules/')) {
+      assert(
+        !bundle.needsStableName,
+        'Vendor bundles should not also be entries',
+      );
       // This is the vendor bundle
       return `vendor.${bundle.hashReference}.${bundle.type}`;
     }
@@ -26,9 +42,20 @@ module.exports = new Namer({
       );
     }
 
+    if (workerEntries[projectRelativePath]) {
+      // Workers need to be positioned exactly as they are loaded via
+      // path.join
+      return workerEntries[projectRelativePath](bundle.hashReference);
+    }
+
     if (projectRelativePath.endsWith('.json')) {
       // JSON files are actually compiled to JS files
       projectRelativePath = projectRelativePath + '.js';
+    }
+
+    if (bundle.needsStableName) {
+      // This is an entry so just use it's base file name
+      return path.basename(entryAsset.filePath);
     }
 
     // This must be another Atlaspack import so just name it to match it's
