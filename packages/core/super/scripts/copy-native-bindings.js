970228e6226a92604d0fc3344d7e3f403aec96fb
Matt Jones <mjones4@atlassian.com>
2025-04-24 12:12:38 +1000

    Fix native bindings for lightning and swc

--- packages/core/super/scripts/copy-native-bindings.js
+++ packages/core/super/scripts/copy-native-bindings.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let fs = require('fs/promises');
+let glob = require('fast-glob');
 let findUp = require('find-up');
 
 let lib_node_modules_dir = path.join(__dirname, '../lib/node_modules');
@@ -7,20 +8,26 @@ let lib_node_modules_dir = path.join(__dirname, '../lib/node_modules');
 let bindings = [
   '@atlaspack/rust',
   '@parcel/source-map',
-  'lightningcss',
-  '@swc/core',
+  // The following bindings use optional deps for native bindings
+  'lightningcss*',
+  '@swc/core*',
   // Uses dynamic requires internally so it can't easily be compiled
   'htmlnano',
 ];
 
 async function main() {
-  for (let package of bindings) {
-    let packageDir = path.dirname(
-      await findUp('package.json', {
-        cwd: require.resolve(package),
-      }),
-    );
-    let target = path.join(lib_node_modules_dir, package);
+  let packageJsons = await glob(
+    bindings.map((binding) => `node_modules/${binding}/package.json`),
+    {
+      cwd: path.join(__dirname, '../../../..'),
+      absolute: true,
+    },
+  );
+
+  for (let packageJson of packageJsons) {
+    let packageName = require(packageJson).name;
+    let packageDir = await fs.realpath(path.dirname(packageJson));
+    let target = path.join(lib_node_modules_dir, packageName);
 
     await fs.cp(packageDir, target, {recursive: true});
   }
