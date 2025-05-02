5f992faf04f3acb0bc3c778f829c79bbe5e62394
Grant Wong <gwong2@atlassian.com>
2025-05-02 12:03:10 +1000

    Fix PluginDocs name

--- packages/core/eslint-plugin-public/src/utils/index.ts
+++ packages/core/eslint-plugin-public/src/utils/index.ts
@@ -1,11 +1,11 @@
 import {ESLintUtils} from '@typescript-eslint/utils';
 
-export interface ExamplePluginDocs {
+export interface PluginDocs {
   description: string;
   recommended?: boolean;
 }
 
-export const createRule = ESLintUtils.RuleCreator<ExamplePluginDocs>(
+export const createRule = ESLintUtils.RuleCreator<PluginDocs>(
   (name) =>
     `https://github.com/atlassian-labs/atlaspack/tree/main/packages/core/eslint-plugin-public/src/rules/${name}/README.md`,
 );
