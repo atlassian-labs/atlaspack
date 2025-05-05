3a587029124601f8578afe85cbe36d50ee182837
yamadapc <tacla.yamada@gmail.com>
2025-05-05 11:04:55 +1000

    Do not cross-compile for arm

--- scripts/debug-symbols.mjs
+++ scripts/debug-symbols.mjs
@@ -1,46 +1,35 @@
 /* eslint-disable no-console */
-import * as path from 'node:path';
-import * as process from 'node:process';
+import * as path from "node:path";
+import * as process from "node:process";
 import * as child_process from 'node:child_process';
-import * as url from 'node:url';
-import glob from 'glob';
+import * as url from "node:url";
+import glob from "glob";
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const __root = path.dirname(__dirname);
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const __root = path.dirname(__dirname)
 
-const objcopy = process.env.OBJCOPY || 'objcopy';
-
-for (const foundRel of glob.sync('packages/**/*.node', {cwd: __root})) {
-  const found = path.join(__root, foundRel);
+for (const foundRel of glob.sync("packages/**/*.node", { cwd: __root })) {
+  const found = path.join(__root, foundRel)
   if (process.platform === 'linux') {
-    console.log(`Stripping:     ${found}`);
-    cmd(
-      `${objcopy} --only-keep-debug --compress-debug-sections=zlib ${found} ${found}.debug`,
-    );
-    cmd(`${objcopy} --strip-debug --strip-unneeded ${found}`);
-    cmd(`${objcopy} --add-gnu-debuglink=${found}.debug ${found}`);
-    console.log(`  ➜ Generated: ${found}.debug`);
+    console.log(`Stripping:     ${found}`)
+    cmd(`objcopy --only-keep-debug --compress-debug-sections=zlib ${found} ${found}.debug`)
+    cmd(`objcopy --strip-debug --strip-unneeded ${found}`)
+    cmd(`objcopy --add-gnu-debuglink=${found}.debug ${found}`)
+    console.log(`  ➜ Generated: ${found}.debug`)
   }
   if (process.platform === 'darwin') {
-    cmd(`dsymutil ${found}`);
-    cmd(`strip -x ${found}`);
+    cmd(`dsymutil ${found}`)
+    cmd(`strip -x ${found}`)
   }
 }
 
 function cmd(command, options) {
   try {
-    const [arg0, ...args] = command
-      .split(' ')
-      .filter((v) => v !== '')
-      .map((v) => v.trim());
-    child_process.execFileSync(arg0, args, {
-      stdio: 'inherit',
-      shell: true,
-      ...options,
-    });
+    const [arg0, ...args] = command.split(' ').filter(v => v !== '').map(v => v.trim())
+    child_process.execFileSync(arg0, args, { stdio: 'inherit', shell: true, ...options })
   } catch (error) {
-    console.error(`Failed: ${command}`);
-    console.error(error);
-    process.exit(1);
+    console.error(`Failed: ${command}`)
+    console.error(error)
+    process.exit(1)
   }
 }
