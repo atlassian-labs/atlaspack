d95d8d6c14f4ac31ec230e3728aece2a7ea67714
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-05-16 12:45:48 +1000

    Chore: Add options to sentry script (#505)

--- scripts/debug-symbols.mjs
+++ scripts/debug-symbols.mjs
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import * as path from 'node:path';
+import * as fs from 'node:fs';
 import * as process from 'node:process';
 import * as url from 'node:url';
 import {$} from 'zx';
@@ -8,7 +9,28 @@ import glob from 'glob';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const __root = path.dirname(__dirname);
 
-async function uploadDebugSymbolsToSentry() {
+const rm = process.argv.includes('--rm');
+const upload = process.argv.includes('--upload-to-sentry');
+
+console.table({
+  'Strip Debug Symbols': true,
+  'Remove After': rm,
+  'Upload to Sentry': upload,
+});
+
+void (async function main() {
+  const debugFiles = await stripDebugSymbols();
+  if (upload) {
+    await uploadDebugSymbolsToSentry();
+  }
+  if (rm) {
+    await removeFiles(debugFiles);
+  }
+})();
+
+/// Find cdylib files and extract their debug symbols into
+/// a separate debug file
+async function stripDebugSymbols() {
   $.stdio = 'inherit';
 
   const debugFiles = [];
@@ -18,10 +40,11 @@ async function uploadDebugSymbolsToSentry() {
     ignore: '**/node_modules/**',
   })) {
     const found = path.join(__root, foundRel);
-    console.log(`Stripping:     ${found}`);
+    console.log('Stripping', found);
 
     if (process.platform === 'linux') {
       const output = `${found}.debug`;
+      console.log('Generating', output);
 
       await $`objcopy --only-keep-debug --compress-debug-sections=zlib ${found} ${output}`;
       await $`objcopy --strip-debug --strip-unneeded ${found}`;
@@ -30,6 +53,7 @@ async function uploadDebugSymbolsToSentry() {
       debugFiles.push(output);
     } else if (process.platform === 'darwin') {
       const dsymOutput = `${found}.dsym`;
+      console.log('Generating', dsymOutput);
 
       await $`dsymutil ${found} -o ${dsymOutput}`;
       await $`strip -x ${found}`;
@@ -38,13 +62,21 @@ async function uploadDebugSymbolsToSentry() {
     }
   }
 
+  return debugFiles;
+}
+
+/// Upload a list of debug files to sentry
+async function uploadDebugSymbolsToSentry() {
+  $.stdio = 'inherit';
   console.log('Uploading debug files to sentry');
   await $`yarn sentry-cli debug-files upload --include-sources --log-level=info packages`;
+}
 
+/// Remove/clean up debug files
+async function removeFiles(debugFiles) {
   for (const debugFile of debugFiles) {
-    await $`rm -rf ${debugFile}`;
+    console.log('Deleting', debugFile);
+    await fs.promises.rm(debugFile, {recursive: true, force: true});
     console.log('Deleted', debugFile);
   }
 }
-
-uploadDebugSymbolsToSentry();
