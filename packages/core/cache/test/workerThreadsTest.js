a7191e1295e8cbab76c84353f7a9e0283aabba05
yamadapc <tacla.yamada@gmail.com>
2025-06-03 15:15:45 +1000

    fix running root test suite
    Created using spr 1.3.7-beta.1

--- packages/core/cache/test/workerThreadsTest.js
+++ packages/core/cache/test/workerThreadsTest.js
@@ -1,34 +1,44 @@
+/* eslint-disable no-inner-declarations */
+
 require('@atlaspack/babel-register');
-const {workerData, threadId, parentPort} = require('worker_threads');
+const {
+  workerData,
+  threadId,
+  parentPort,
+  isMainThread,
+} = require('worker_threads');
 const {LMDBLiteCache} = require('../src/index');
 
-const cache = new LMDBLiteCache(workerData.cacheDir);
-async function onMessage() {
-  try {
-    cache.set(`worker_key/${threadId}`, {
-      workerId: threadId,
-    });
+if (!isMainThread) {
+  const cache = new LMDBLiteCache(workerData.cacheDir);
 
-    const data = await cache.get('main_thread_key');
-
-    parentPort.postMessage({
-      mainThreadData: data,
-      workerId: threadId,
-    });
-
-    setTimeout(() => {
-      cache.getNativeRef().close();
-
-      parentPort.postMessage({
-        type: 'close',
+  async function onMessage() {
+    try {
+      cache.set(`worker_key/${threadId}`, {
         workerId: threadId,
       });
-    }, Math.random() * 200);
-  } catch (error) {
-    parentPort.postMessage({
-      error: error.message,
-    });
-  }
-}
 
-parentPort.on('message', onMessage);
+      const data = await cache.get('main_thread_key');
+
+      parentPort.postMessage({
+        mainThreadData: data,
+        workerId: threadId,
+      });
+
+      setTimeout(() => {
+        cache.getNativeRef().close();
+
+        parentPort.postMessage({
+          type: 'close',
+          workerId: threadId,
+        });
+      }, Math.random() * 200);
+    } catch (error) {
+      parentPort.postMessage({
+        error: error.message,
+      });
+    }
+  }
+
+  parentPort.on('message', onMessage);
+}
