1f4224a894ab022f34e6002e9df987d9269aa8da
Matt Jones <mjones4@atlassian.com>
2025-06-06 12:15:11 +1000

    WIP

--- packages/bundlers/default/src/bundleMerge.js
+++ packages/bundlers/default/src/bundleMerge.js
@@ -4,133 +4,251 @@ import invariant from 'assert';
 import nullthrows from 'nullthrows';
 import type {NodeId} from '@atlaspack/graph';
 import type {Bundle, IdealBundleGraph} from './idealGraph';
-import {ContentGraph} from '@atlaspack/graph';
+import {ContentGraph, BitSet} from '@atlaspack/graph';
+import {memoize, clearCaches} from './memoize';
+
+function getBundlesForBundleGroup(
+  bundleGraph: IdealBundleGraph,
+  bundleGroupId: NodeId,
+): number {
+  let count = 0;
+  bundleGraph.traverse((nodeId) => {
+    if (bundleGraph.getNode(nodeId)?.bundleBehavior !== 'inline') {
+      count++;
+    }
+  }, bundleGroupId);
+  return count;
+}
+
+let getBundleOverlapBitSet = (
+  sourceBundlesA: BitSet,
+  sourceBundlesB: BitSet,
+): number => {
+  let allSourceBundles = BitSet.union(sourceBundlesA, sourceBundlesB);
+  let sharedSourceBundles = BitSet.intersect(sourceBundlesA, sourceBundlesB);
+
+  return sharedSourceBundles.size() / allSourceBundles.size();
+};
 
 // Returns a decimal showing the proportion source bundles are common to
 // both bundles versus the total number of source bundles.
 function checkBundleThreshold(
-  bundleA: Bundle,
-  bundleB: Bundle,
+  bundleA: MergeCandidate,
+  bundleB: MergeCandidate,
   threshold: number,
 ): boolean {
-  let sharedSourceBundles = 0;
-  let allSourceBundles = new Set([
-    ...bundleA.sourceBundles,
-    ...bundleB.sourceBundles,
-  ]);
-
-  for (let bundle of bundleB.sourceBundles) {
-    if (bundleA.sourceBundles.has(bundle)) {
-      sharedSourceBundles++;
-    }
-  }
-
-  let score = sharedSourceBundles / allSourceBundles.size;
-  return score >= threshold;
+  return (
+    getBundleOverlapBitSet(
+      bundleA.sourceBundleBitSet,
+      bundleB.sourceBundleBitSet,
+    ) >= threshold
+  );
 }
 
-function checkAncestorOverlap(
-  bundleA: Bundle,
-  bundleB: Bundle,
-  importantAncestorBundles: Array<Array<NodeId>>,
-): boolean {
-  if (importantAncestorBundles.length === 0) {
+let checkAncestorOverlap = memoize(
+  (bundle: Bundle, importantAncestorBundles: Array<NodeId>): boolean => {
+    return importantAncestorBundles.every((ancestorId) =>
+      bundle.sourceBundles.has(ancestorId),
+    );
+  },
+);
+
+let hasSuitableBundleGroup = memoize(
+  (
+    bundleGraph: IdealBundleGraph,
+    bundle: Bundle,
+    minBundlesInGroup: number,
+  ): boolean => {
+    for (let sourceBundle of bundle.sourceBundles) {
+      let bundlesInGroup = getBundlesForBundleGroup(bundleGraph, sourceBundle);
+
+      if (bundlesInGroup >= minBundlesInGroup) {
+        return true;
+      }
+    }
     return false;
-  }
+  },
+);
 
-  for (let ancestorBundle of importantAncestorBundles) {
-    let bundleAHasAllAncestors = ancestorBundle.every((ancestorId) =>
-      bundleA.sourceBundles.has(ancestorId),
-    );
-
-    let bundleBHasAllAncestors = ancestorBundle.every((ancestorId) =>
-      bundleB.sourceBundles.has(ancestorId),
-    );
-
-    if (bundleAHasAllAncestors && bundleBHasAllAncestors) {
-      return true;
+function validMerge(
+  bundleGraph: IdealBundleGraph,
+  config: MergeGroup,
+  bundleA: MergeCandidate,
+  bundleB: MergeCandidate,
+): boolean {
+  if (config.maxBundleSize != null) {
+    if (
+      bundleA.bundle.size > config.maxBundleSize ||
+      bundleB.bundle.size > config.maxBundleSize
+    ) {
+      return false;
     }
   }
 
-  return false;
+  if (config.overlapThreshold != null) {
+    if (!checkBundleThreshold(bundleA, bundleB, config.overlapThreshold)) {
+      return false;
+    }
+  }
+
+  if (config.importantAncestorBundles != null) {
+    if (
+      !checkAncestorOverlap(bundleA.bundle, config.importantAncestorBundles) ||
+      !checkAncestorOverlap(bundleB.bundle, config.importantAncestorBundles)
+    ) {
+      return false;
+    }
+  }
+
+  if (config.minBundlesInGroup != null) {
+    if (
+      !hasSuitableBundleGroup(
+        bundleGraph,
+        bundleA.bundle,
+        config.minBundlesInGroup,
+      ) ||
+      !hasSuitableBundleGroup(
+        bundleGraph,
+        bundleB.bundle,
+        config.minBundlesInGroup,
+      )
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function getMergeClusters(
-  graph: ContentGraph<NodeId>,
-  candidates: Set<NodeId>,
+  graph: ContentGraph<NodeId, EdgeType>,
+  candidates: Map<NodeId, EdgeType>,
 ): Array<Array<NodeId>> {
   let clusters = [];
 
-  for (let candidate of candidates) {
+  for (let [candidate, edgeType] of candidates.entries()) {
     let cluster: Array<NodeId> = [];
 
-    graph.traverse((nodeId) => {
-      cluster.push(nullthrows(graph.getNode(nodeId)));
-      // Remove node from candidates as it has already been processed
-      candidates.delete(nodeId);
-    }, candidate);
-
+    graph.traverse(
+      (nodeId) => {
+        cluster.push(nullthrows(graph.getNode(nodeId)));
+        // Remove node from candidates as it has already been processed
+        candidates.delete(nodeId);
+      },
+      candidate,
+      edgeType,
+    );
     clusters.push(cluster);
   }
 
   return clusters;
 }
 
-export function findMergeCandidates(
+type MergeCandidate = {|
+  bundle: Bundle,
+  id: NodeId,
+  sourceBundleBitSet: BitSet,
+  contentKey: string,
+|};
+function getPossibleMergeCandidates(
   bundleGraph: IdealBundleGraph,
   bundles: Array<NodeId>,
-  threshold: number,
-  importantAncestorBundles: Array<Array<NodeId>> = [],
-): Array<Array<NodeId>> {
-  let graph = new ContentGraph<NodeId>();
-  let seen = new Set<string>();
-  let candidates = new Set<NodeId>();
-
-  // Build graph of clustered merge candidates
-  for (let bundleId of bundles) {
+): Array<[MergeCandidate, MergeCandidate]> {
+  let mergeCandidates = bundles.map((bundleId) => {
     let bundle = bundleGraph.getNode(bundleId);
-    invariant(bundle && bundle !== 'root');
-    if (bundle.type !== 'js') {
-      continue;
+    invariant(bundle && bundle !== 'root', 'Bundle should exist');
+
+    let sourceBundleBitSet = new BitSet(bundleGraph.nodes.length);
+    for (let sourceBundle of bundle.sourceBundles) {
+      sourceBundleBitSet.add(sourceBundle);
     }
 
-    for (let otherBundleId of bundles) {
-      if (bundleId === otherBundleId) {
-        continue;
-      }
+    return {
+      id: bundleId,
+      bundle,
+      sourceBundleBitSet,
+      contentKey: bundleId.toString(),
+    };
+  });
 
-      let key = [bundleId, otherBundleId].sort().join(':');
+  const uniquePairs = [];
 
-      if (seen.has(key)) {
-        continue;
-      }
-      seen.add(key);
-
-      let otherBundle = bundleGraph.getNode(otherBundleId);
-      invariant(otherBundle && otherBundle !== 'root');
+  for (let i = 0; i < mergeCandidates.length; i++) {
+    for (let j = i + 1; j < mergeCandidates.length; j++) {
+      let a = mergeCandidates[i];
+      let b = mergeCandidates[j];
 
       if (
-        checkBundleThreshold(bundle, otherBundle, threshold) ||
-        checkAncestorOverlap(bundle, otherBundle, importantAncestorBundles)
+        // $FlowFixMe both bundles will always have internalizedAssets
+        a.bundle.internalizedAssets.equal(b.bundle.internalizedAssets)
       ) {
-        let bundleNode = graph.addNodeByContentKeyIfNeeded(
-          bundleId.toString(),
-          bundleId,
-        );
-        let otherBundleNode = graph.addNodeByContentKeyIfNeeded(
-          otherBundleId.toString(),
-          otherBundleId,
-        );
-
-        // Add edge in both directions
-        graph.addEdge(bundleNode, otherBundleNode);
-        graph.addEdge(otherBundleNode, bundleNode);
-
-        candidates.add(bundleNode);
-        candidates.add(otherBundleNode);
+        uniquePairs.push([a, b]);
       }
     }
   }
+  return uniquePairs;
+}
+
+type MergeGroup = {|
+  overlapThreshold?: number,
+  maxBundleSize?: number,
+  importantAncestorBundles?: Array<NodeId>,
+  minBundlesInGroup?: number,
+|};
+type EdgeType = number;
+
+export function findMergeCandidates(
+  bundleGraph: IdealBundleGraph,
+  bundles: Array<NodeId>,
+  config: Array<MergeGroup>,
+): Array<Array<NodeId>> {
+  let graph = new ContentGraph<NodeId, EdgeType>();
+  let candidates = new Map<NodeId, EdgeType>();
+
+  let allPossibleMergeCandidates = getPossibleMergeCandidates(
+    bundleGraph,
+    bundles,
+  );
+
+  // Build graph of clustered merge candidates
+  for (let i = 0; i < config.length; i++) {
+    // Ensure edge type coresponds to config index
+    let edgeType = i + 1;
+
+    for (let group of allPossibleMergeCandidates) {
+      let candidateA = group[0];
+      let candidateB = group[1];
+
+      if (!validMerge(bundleGraph, config[i], candidateA, candidateB)) {
+        continue;
+      }
+
+      let bundleNode = graph.addNodeByContentKeyIfNeeded(
+        candidateA.contentKey,
+        candidateA.id,
+      );
+      let otherBundleNode = graph.addNodeByContentKeyIfNeeded(
+        candidateB.contentKey,
+        candidateB.id,
+      );
+
+      // Add edge in both directions
+      graph.addEdge(bundleNode, otherBundleNode, edgeType);
+      graph.addEdge(otherBundleNode, bundleNode, edgeType);
+
+      candidates.set(bundleNode, edgeType);
+      candidates.set(otherBundleNode, edgeType);
+    }
+
+    // Remove bundles that have been allocated to a higher priority merge
+    allPossibleMergeCandidates = allPossibleMergeCandidates.filter(
+      (group) =>
+        !graph.hasContentKey(group[0].contentKey) &&
+        !graph.hasContentKey(group[1].contentKey),
+    );
+  }
+
+  clearCaches();
 
   return getMergeClusters(graph, candidates);
 }
