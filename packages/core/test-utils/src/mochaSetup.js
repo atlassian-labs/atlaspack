6ec3f688ef4f6159fcf68e3ce26eb19025a7d62b
Marcin Szczepanski <mszczepanski@atlassian.com>
2025-07-01 18:30:11 +1000

    Fix formatting of debug message

--- packages/core/test-utils/src/mochaSetup.js
+++ packages/core/test-utils/src/mochaSetup.js
@@ -1,10 +1,10 @@
 if (process.env.ATLASPACK_MOCHA_HANG_DEBUG === 'true') {
   const whyIsNodeRunning = require('why-is-node-running').default;
   // eslint-disable-next-line no-console
-  console.log(`\n\n🛠️ Mocha process PID: ${process.pid}`);
+  console.log(`\n\n🛠️  Mocha process PID: ${process.pid}`);
   // eslint-disable-next-line no-console
   console.log(
-    `🛠️ Run 'kill -SIGHUP ${process.pid}' to get information about open handles\n\n`,
+    `🛠️  Run 'kill -SIGHUP ${process.pid}' to get information about open handles\n\n`,
   );
 
   process.on('SIGHUP', () => {
