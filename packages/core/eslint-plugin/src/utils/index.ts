fc06821540496900d87747f4d465667c43404365
Liam Ma <liam.q.ma@gmail.com>
2025-07-04 09:39:10 +1000

    Remove unneeded changes

--- packages/core/eslint-plugin/src/utils/index.ts
+++ packages/core/eslint-plugin/src/utils/index.ts
@@ -9,6 +9,3 @@ export const createRule = ESLintUtils.RuleCreator<PluginDocs>(
   (name) =>
     `https://github.com/atlassian-labs/atlaspack/tree/main/packages/core/eslint-plugin/src/rules/${name}/README.md`,
 );
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type SomeType = any; // replace with actual type export
