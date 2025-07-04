de23e0ce49d5504fe3947ac26640a3d951087da3
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-07-04 10:56:25 +0700

    Run CI integration tests against compiled Atlaspack (#645)
    Co-authored-by: Liam Ma <lma2@atlassian.com>

--- packages/core/cli/src/bin.js
+++ packages/core/cli/src/bin.js
@@ -3,7 +3,8 @@
 'use strict';
 
 if (
-  process.env.ATLASPACK_BUILD_ENV !== 'production' ||
+  process.env.ATLASPACK_SOURCES === 'true' ||
+  process.env.ATLASPACK_BUILD_ENV === 'test' ||
   process.env.ATLASPACK_SELF_BUILD
 ) {
   require('@atlaspack/babel-register');
