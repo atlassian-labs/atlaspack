26070c871d2700daf4501051dd87b58b414912c0
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-07-10 06:58:23 +0700

    Fix CLI for local development (#687)

--- packages/core/cli/bin/atlaspack.js
+++ packages/core/cli/bin/atlaspack.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-require('../lib/bin');
+'use strict';
+
+if (
+  process.env.ATLASPACK_REGISTER_USE_SRC === 'true' ||
+  process.env.ATLASPACK_BUILD_ENV === 'test' ||
+  process.env.ATLASPACK_SELF_BUILD
+) {
+  require('@atlaspack/babel-register');
+  require('../src/cli');
+} else {
+  require('../lib/cli');
+}
