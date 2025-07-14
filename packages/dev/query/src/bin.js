7dbde328159bbda1abaf6f14a6b7da33b6faa4ae
David Alsh <alshdavid@gmail.com>
2025-07-14 13:59:50 +1000

    ok tests are at least running now

--- packages/dev/query/src/bin.js
+++ packages/dev/query/src/bin.js
@@ -2,14 +2,6 @@
 
 'use strict';
 
-if (
-  process.env.ATLASPACK_SOURCES === 'true' ||
-  process.env.ATLASPACK_BUILD_ENV === 'test' ||
-  process.env.ATLASPACK_SELF_BUILD
-) {
-  require('@atlaspack/babel-register');
-}
-
 const run = require('./cli').run;
 require('v8-compile-cache');
 
