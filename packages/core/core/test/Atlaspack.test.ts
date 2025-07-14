5554dd2e29d15c0e5990a99b865039a7165a2195
David Alsh <alshdavid@gmail.com>
2025-07-14 15:18:57 +1000

    tests

--- packages/core/core/test/Atlaspack.test.ts
+++ packages/core/core/test/Atlaspack.test.ts
@@ -130,7 +130,8 @@ function createAtlaspack(opts?: InitialAtlaspackOptions) {
     entries: [path.join(__dirname, 'fixtures/atlaspack/index.js')],
     logLevel: 'info',
     defaultConfig: path.join(
-      path.dirname(require.resolve('@atlaspack/test-utils')),
+      path.dirname(path.dirname(require.resolve('@atlaspack/test-utils'))),
+      'configs',
       '.parcelrc-no-reporters',
     ),
     shouldDisableCache: true,
