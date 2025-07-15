dd1ac9af2b21cd9ed8f2604507c2fd87637fa822
Carl Sowden <csowden@atlassian.com>
2025-07-15 16:07:16 +1000

    update invalidateOnOptionChange now type has changed

--- packages/core/core/src/Transformation.js
+++ packages/core/core/src/Transformation.js
@@ -123,14 +123,7 @@ export default class Transformation {
       optionsProxy(
         this.options,
         (option) => {
-          // Check if we're using the new granular option tracking (path arrays)
-          if (Array.isArray(option)) {
-            // Convert array paths to dot-notation for compatibility with Set<string>
-            this.invalidations.invalidateOnOptionChange.add(option.join('.'));
-          } else {
-            // Original behavior for string paths
-            this.invalidations.invalidateOnOptionChange.add(option);
-          }
+          this.invalidations.invalidateOnOptionChange.add(option);
         },
         (devDep) => {
           this.pluginDevDeps.push(devDep);
