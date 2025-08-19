091929d25eb8e4c2690ce516b77ce6a77c55cc04
mszczepanski <mszczepanski@atlassian.com>
2025-08-19 15:17:17 +1000

    Fix types in WebExtensionRuntime

--- packages/transformers/typescript-tsc/src/TSCTransformer.ts
+++ packages/transformers/typescript-tsc/src/TSCTransformer.ts
@@ -44,7 +44,7 @@ export default new Transformer({
       let map = new SourceMap(options.projectRoot);
       map.addVLQMap(JSON.parse(sourceMapText));
       if (originalMap) {
-        // @ts-expect-error TS2345
+        // @ts-expect-error TS2345 - the types are wrong, `extends` accepts a `SourceMap` or a `Buffer`
         map.extends(originalMap);
       }
       asset.setMap(map);
