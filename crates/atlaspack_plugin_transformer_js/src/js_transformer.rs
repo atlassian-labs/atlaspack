d8b2cdf9ae0e121419bbb819009aa2047b1c0f6d
mszczepanski <mszczepanski@atlassian.com>
2025-08-29 16:08:07 +1000

    Fix more tests depending on environment ids

--- crates/atlaspack_plugin_transformer_js/src/js_transformer.rs
+++ crates/atlaspack_plugin_transformer_js/src/js_transformer.rs
@@ -558,7 +558,7 @@ mod tests {
           file_type: FileType::Js,
           // TODO: Is this correct?
           symbols: Some(vec![Symbol {
-            local: String::from("$794b991511cb8fe6$exports"),
+            local: String::from("$e649e1a37d1ddc23$exports"),
             exported: String::from("*"),
             loc: None,
             is_weak: false,
@@ -766,7 +766,7 @@ mod tests {
       symbols: Some(vec![Symbol {
         exported: String::from("*"),
         loc: None,
-        local: String::from("a1ad9714284f3ad6$"),
+        local: String::from("78f713213cef1ff3$"),
         ..Symbol::default()
       }]),
       ..Default::default()
