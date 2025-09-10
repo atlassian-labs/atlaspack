625fc66aa60ec26d77e93df22954246211c7d813
matt-koko <matthew@kokolich.com>
2025-09-11 01:20:36 +1000

    fix the rewrite-package-types.mjs script to point to the correct location where TypeScript actually outputs the declaration files.

--- scripts/rewrite-package-types.mjs
+++ scripts/rewrite-package-types.mjs
@@ -21,7 +21,7 @@ for (const workspace of rootPkg.workspaces) {
     if (!pkg.source) continue;
     if (pkg.source != pkg.types) continue;
 
-    pkg.types = pkg.types.replace('./src/', './lib/').replace('.ts', '.d.ts');
+    pkg.types = pkg.types.replace('./src/', './lib/types/').replace('.ts', '.d.ts');
 
     fs.writeFileSync(abs, JSON.stringify(pkg, null, 2), 'utf8');
   }
