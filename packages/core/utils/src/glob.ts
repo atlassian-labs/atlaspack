b1801784104936df583532b24b7c776da98a1f49
Ben Jervis <bjervis@atlassian.com>
2025-09-22 10:43:10 +1000

    Improve some types

--- packages/core/utils/src/glob.ts
+++ packages/core/utils/src/glob.ts
@@ -1,10 +1,7 @@
-import type {FilePath, Glob} from '@atlaspack/types-internal';
-import type {FileSystem} from '@atlaspack/types-internal';
+import type {FilePath, FileSystem, Glob} from '@atlaspack/types-internal';
 
-// @ts-expect-error TS7016
 import _isGlob from 'is-glob';
-// @ts-expect-error TS2305
-import fastGlob, {FastGlobOptions} from 'fast-glob';
+import fastGlob, {Options as FastGlobOptions} from 'fast-glob';
 import micromatch, {isMatch, makeRe, type Options} from 'micromatch';
 import {normalizeSeparators} from './path';
 
@@ -42,16 +39,14 @@ export function globToRegex(glob: Glob, opts?: Options): RegExp {
 export function globSync(
   p: FilePath,
   fs: FileSystem,
-  options?: FastGlobOptions<FilePath>,
+  options?: FastGlobOptions,
 ): Array<FilePath> {
   options = {
     ...options,
     fs: {
-      // @ts-expect-error TS7006
       statSync: (p) => {
         return fs.statSync(p);
       },
-      // @ts-expect-error TS7006
       lstatSync: (p) => {
         // Our FileSystem interface doesn't have lstat support at the moment,
         // but this is fine for our purposes since we follow symlinks by default.
@@ -64,33 +59,32 @@ export function globSync(
     },
   };
 
-  // @ts-expect-error TS2322
   return fastGlob.sync(normalizeSeparators(p), options);
 }
 
 export function glob(
   p: FilePath,
   fs: FileSystem,
-  options: FastGlobOptions<FilePath>,
+  options: FastGlobOptions,
 ): Promise<Array<FilePath>> {
   options = {
     ...options,
     fs: {
-      // @ts-expect-error TS7006
       stat: async (p, cb) => {
         try {
           cb(null, await fs.stat(p));
         } catch (err: any) {
+          // @ts-expect-error TS2345
           cb(err);
         }
       },
-      // @ts-expect-error TS7006
       lstat: async (p, cb) => {
         // Our FileSystem interface doesn't have lstat support at the moment,
         // but this is fine for our purposes since we follow symlinks by default.
         try {
           cb(null, await fs.stat(p));
         } catch (err: any) {
+          // @ts-expect-error TS2345
           cb(err);
         }
       },
@@ -98,18 +92,19 @@ export function glob(
       readdir: async (p, opts, cb) => {
         if (typeof opts === 'function') {
           cb = opts;
+          // @ts-expect-error TS2322
           opts = null;
         }
 
         try {
           cb(null, await fs.readdir(p, opts));
         } catch (err: any) {
+          // @ts-expect-error TS2345
           cb(err);
         }
       },
     },
   };
 
-  // @ts-expect-error TS2322
   return fastGlob(normalizeSeparators(p), options);
 }
