f9d9448b2c19e96a87e5b357755c10b7245d4302
Marcin Szczepanski <mszczepanski@atlassian.com>
2025-09-26 15:20:43 +1000

    Initial implementation of native tokens plugin

--- packages/transformers/js/src/JSTransformer.ts
+++ packages/transformers/js/src/JSTransformer.ts
@@ -117,6 +117,32 @@ const CONFIG_SCHEMA: SchemaEntity = {
     unstable_inlineConstants: {
       type: 'boolean',
     },
+    atlaskitTokens: {
+      type: 'object',
+      properties: {
+        shouldUseAutoFallback: {
+          type: 'boolean',
+        },
+        shouldForceAutoFallback: {
+          type: 'boolean',
+        },
+        forceAutoFallbackExemptions: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+        defaultTheme: {
+          type: 'string',
+          enum: ['light', 'legacy-light'],
+        },
+        tokenDataPath: {
+          type: 'string',
+        },
+      },
+      required: ['tokenDataPath'],
+      additionalProperties: false,
+    },
   },
   additionalProperties: false,
 };
@@ -302,6 +328,7 @@ export default new Transformer({
     let inlineConstants = false;
     let magicComments = false;
     let addReactDisplayName = false;
+    let atlaskitTokens;
 
     if (conf && conf.contents) {
       validateSchema.diagnostic(
@@ -330,6 +357,20 @@ export default new Transformer({
       inlineConstants =
         // @ts-expect-error TS2339
         conf.contents?.unstable_inlineConstants ?? inlineConstants;
+
+      // @ts-expect-error TS2339
+      if (conf.contents?.atlaskitTokens) {
+        // @ts-expect-error TS2339
+        const tokensConfig = conf.contents.atlaskitTokens;
+        atlaskitTokens = {
+          shouldUseAutoFallback: tokensConfig.shouldUseAutoFallback ?? true,
+          shouldForceAutoFallback: tokensConfig.shouldForceAutoFallback ?? true,
+          forceAutoFallbackExemptions:
+            tokensConfig.forceAutoFallbackExemptions ?? [],
+          defaultTheme: tokensConfig.defaultTheme ?? 'light',
+          tokenDataPath: tokensConfig.tokenDataPath, // Required by schema validation
+        };
+      }
     }
 
     return {
@@ -346,6 +387,7 @@ export default new Transformer({
       decorators,
       useDefineForClassFields,
       magicComments,
+      atlaskitTokens,
     };
   },
   async transform({asset, config, options, logger}) {
@@ -524,6 +566,18 @@ export default new Transformer({
       magic_comments:
         Boolean(config?.magicComments) ||
         getFeatureFlag('supportWebpackChunkName'),
+      is_source: asset.isSource,
+      ...(config?.atlaskitTokens && {
+        atlaskit_tokens: {
+          should_use_auto_fallback: config.atlaskitTokens.shouldUseAutoFallback,
+          should_force_auto_fallback:
+            config.atlaskitTokens.shouldForceAutoFallback,
+          force_auto_fallback_exemptions:
+            config.atlaskitTokens.forceAutoFallbackExemptions,
+          default_theme: config.atlaskitTokens.defaultTheme,
+          token_data_path: config.atlaskitTokens.tokenDataPath, // Now required
+        },
+      }),
       callMacro: asset.isSource
         ? async (err: any, src: any, exportName: any, args: any, loc: any) => {
             let mod;
