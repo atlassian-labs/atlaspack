8d9c3758407545968869a7e2050c3ba5ecd77407
Matt Jones <mjones4@atlassian.com>
2025-10-14 09:16:07 +1100

    Formatting

--- crates/atlaspack/src/plugins/plugin_cache.rs
+++ crates/atlaspack/src/plugins/plugin_cache.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashMap, sync::Arc};
 use std::future::Future;
+use std::{collections::HashMap, sync::Arc};
 
 use atlaspack_core::plugin::{ResolverPlugin, TransformerPlugin};
 use once_cell::sync::OnceCell;
