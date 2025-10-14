d0c7bea04458255b4c5d4299600e734b5f919fe1
vykimnguyen <vnguyen4@atlassian.com>
2025-10-15 10:05:09 +1100

    Ensure unique reporters from config (#844)

--- crates/atlaspack/src/atlaspack.rs
+++ crates/atlaspack/src/atlaspack.rs
@@ -94,8 +94,13 @@ impl Atlaspack {
 
     let rpc_worker = rpc.start()?;
 
-    let rc_config_loader =
-      AtlaspackRcConfigLoader::new(Arc::clone(&fs), Arc::clone(&package_manager));
+    let rc_config_loader = AtlaspackRcConfigLoader::new(
+      Arc::clone(&fs),
+      Arc::clone(&package_manager),
+      resolved_options
+        .feature_flags
+        .bool_enabled("deduplicateReporters"),
+    );
 
     let (config, _files) = rc_config_loader.load(
       &project_root,
