5776be21f70a3f2b9471ba33da3ba1a883f21f1a
Marcin Szczepanski <mszczepanski@atlassian.com>
2025-10-23 08:32:38 +1100

    Support deferred source loading for config schema validation (#852)

--- packages/transformers/tokens/src/TokensTransformer.ts
+++ packages/transformers/tokens/src/TokensTransformer.ts
@@ -46,7 +46,9 @@ export default new Transformer({
         CONFIG_SCHEMA,
         {
           data: conf.contents,
-          source: await options.inputFS.readFile(conf.filePath, 'utf8'),
+          source: getFeatureFlag('schemaValidationDeferSourceLoading')
+            ? () => options.inputFS.readFileSync(conf.filePath, 'utf8')
+            : await options.inputFS.readFile(conf.filePath, 'utf8'),
           filePath: conf.filePath,
           prependKey: `/${encodeJSONKeyComponent('@atlaspack/transformer-tokens')}`,
         },
