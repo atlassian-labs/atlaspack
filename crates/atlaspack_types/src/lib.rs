15daf8bfd79dcc6d32bfa6e34c6d87f158dc3a0c
Ben Jervis <bjervis@atlassian.com>
2025-11-19 16:53:29 +1100

    Move types out to their own crate

---
+++ crates/atlaspack_types/src/lib.rs
@@ -0,0 +1,22 @@
+mod asset;
+mod dependency;
+mod environment;
+mod serialization;
+
+// mod atlaspack_options;
+// mod bundle;
+// mod bundle_graph;
+// mod diagnostic;
+// mod environment;
+// mod feature_flags;
+// mod file;
+// mod file_type;
+// mod invalidation;
+// mod json;
+// mod lib;
+// mod package_json;
+// mod source;
+// mod symbol;
+// mod target;
+// mod utils;
+//
