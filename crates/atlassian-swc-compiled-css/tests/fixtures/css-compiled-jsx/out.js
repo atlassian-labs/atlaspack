042cd95a32ad38cd02d57aa3344c36d4e7ab8f9c
Jake Lane <jlane2@atlassian.com>
2025-11-27 01:57:26 +1100

    Add hash gated migration strategy for compiled swc plugin (#887)

--- crates/atlassian-swc-compiled-css/tests/fixtures/css-compiled-jsx/out.js
+++
@@ -1,6 +0,0 @@
-const _ = '._syaz5scu{color:red}';
-import {ax, CC, CS} from '@compiled/react/runtime';
-<CC>
-  <CS>{[_]}</CS>
-  {<div className={ax(['_syaz5scu'])}>hello</div>}
-</CC>;
--- crates/atlassian-swc-compiled-css/tests/fixtures/keyframes-styled/in.jsx
+++
@@ -1,9 +0,0 @@
-import { styled, keyframes } from '@compiled/react';
-
-const fade = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } });
-
-const Button = styled.button({
-	animationName: fade,
-});
-
-console.log(Button);
