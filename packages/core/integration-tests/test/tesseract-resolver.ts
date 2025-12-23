265b72d8a69b42531aaee1b147a3b42d22493162
Ellie Z <ezeidan@atlassian.com>
2025-12-22 18:16:23 -0600

    update tests

--- packages/core/integration-tests/test/tesseract-resolver.ts
+++ packages/core/integration-tests/test/tesseract-resolver.ts
@@ -10,49 +10,6 @@ import {
 } from '@atlaspack/test-utils';
 
 describe('tesseract-resolver', function () {
-  it('should not fail when resolving protocol-relative URLs in CSS', async function () {
-    // Ensures protocol-relative URLs (//example.com) in CSS are not incorrectly treated as absolute file paths by TesseractResolver.
-    await fsFixture(overlayFS, __dirname)`
-      resolver-protocol-relative-url
-        package.json:
-          {
-            "name": "resolver-protocol-relative-url",
-            "version": "1.0.0"
-          }
-
-        .parcelrc:
-          {
-            "extends": "@atlaspack/config-default",
-            "resolvers": ["@atlaspack/resolver-tesseract"]
-          }
-
-        index.html:
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <style>
-                @font-face {
-                  font-family: 'Test';
-                  src: url('//example.com/font.woff2') format('woff2');
-                }
-              </style>
-            </head>
-            <body>
-              <h1>Test</h1>
-            </body>
-          </html>
-    `;
-
-    let b = await bundle(
-      path.join(__dirname, 'resolver-protocol-relative-url/index.html'),
-      {
-        inputFS: overlayFS,
-      },
-    );
-
-    // The build should succeed without throwing an error
-    assert(b !== null);
-  });
   it('should skip resolving unsupportedExtensions', async function () {
     await fsFixture(overlayFS, __dirname)`
       resolver-protocol-relative-url
